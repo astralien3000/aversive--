ccb3b0a125b7fb003acc3f4e5526d71dfdd83288
Elioty <roadkiller.cl@gmail.com>
2013-12-06 17:27:17 +0100

    Continuation de la mise en place des makefile ; Petit problème avec le ar, le makefile le lance même si les dépendances n'ont pas été modifiées

---
+++ src/sasiae/aversive.cpp
@@ -0,0 +1,9 @@
+#include <aversive.hpp>
+
+void Aversive::init() {
+  return; // Do stuffs with Qt to initialise a thread to manage the communication via a socket to the simulator
+}
+
+void Aversive::sleep(int ms) {
+  return; // Do synchronisation stuff
+}
