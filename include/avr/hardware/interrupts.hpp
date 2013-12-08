1a86e520a7af8f27cd76dc8be92d2a1a25a338c9
Loïc Dauphin <ldauphin@enseirb-matmeca.fr>
2013-12-08 12:55:52 +0100

    add avr interrupts

---
+++ include/avr/hardware/interrupts.hpp
@@ -0,0 +1,24 @@
+#ifndef AVR_INTERRUPTS_HPP
+#define AVR_INTERRUPTS_HPP
+
+#include "../../../common/hardware/interrupts.hpp"
+
+inline void Interrupts::clear(void) {
+  __asm__ __volatile__ ("CLI\n");
+}
+
+inline void Interrupts::set(void) {
+  __asm__ __volatile__ ("SEI\n");
+}
+
+template<typename T> inline void Interrupts::lock(T& flag) {
+  flag = SREG;
+  Interrupts::clear();
+}
+
+template<typename T> inline void Interrupts::unlock(T& flag) {
+  SREG = flag;
+  Interrupts::set();
+}
+
+#endif//AVR_INTERRUPTS_HPP
