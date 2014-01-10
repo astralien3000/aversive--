0cc2d07dc7d89dc29f08a20281acde0ec8c702fb
Elioty <roadkiller.cl@gmail.com>
2014-01-11 00:48:10 +0100

    Including stdlib.h in avr/container/buffer.hpp because, oddly, abort() isn't included in assert.h on AVR

--- include/avr/container/buffer.hpp
+++ include/avr/container/buffer.hpp
@@ -1,6 +1,7 @@
 #ifndef AVR_BUFFER_HPP
 #define AVR_BUFFER_HPP
 
+#include <stdlib.h> // It's needed since, oddly, abort() ins't included in assert.h on AVR while it's on PC.
 #include "../../common/container/buffer.hpp"
 
 
