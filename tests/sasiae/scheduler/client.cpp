48f63085e190be69dc655cce47e77501bd5d0e90
Loïc Dauphin <ldauphin@enseirb-matmeca.fr>
2014-01-31 13:19:21 +0100

    Tests... Forgot to add...

---
+++ tests/sasiae/scheduler/client.cpp
@@ -0,0 +1,41 @@
+#include <aversive.hpp>
+#include <system/scheduler.hpp>
+
+#include <device/output_device.hpp>
+
+OutputDevice<int> out("out");
+
+bool robotInit(void) {
+
+  Task t1([]() {
+      out.setValue(1);
+    });
+
+  Task t2([]() {
+      out.setValue(20);
+    });
+  
+  t1.setPeriod(10);
+  t2.setPeriod(10);
+
+  t1.setRepeat();
+  t2.setRepeat();
+
+  Scheduler::instance().addTask(t1);
+  Scheduler::instance().addTask(t2);
+
+  //Scheduler::instance().rmTask(t1);
+  //Scheduler::instance().rmTask(t2);
+  
+  return 1;
+}
+
+void robotLoop(void) {
+  
+  return;
+}
+
+void robotExit(void) {
+  
+  return;
+}
