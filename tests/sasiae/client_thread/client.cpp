48e929574d1067038a10df2d2c7c3bdb0f273f2e
ldauphin <astralien3000@yahoo.fr>
2014-02-09 11:14:54 +0100

    client_thread sasiae test works !

--- tests/sasiae/client_thread/client.cpp
+++ tests/sasiae/client_thread/client.cpp
@@ -8,9 +8,16 @@
 #include <iostream>
 #include <QThread>
 
-Device mole_device("TESTER");
 
-bool robotInit(void) {
+int main(int argc, char* argv[]) {
+  (void) argc;
+  (void) argv;
+  
+  Aversive::init();
+  // Declare your devices here
+  // Initialize your stuff here
+  Device mole_device("TESTER");
+  
   bool ok = ClientThread::instance().
     registerDevice(mole_device,
 		   std::function<void(char*)>([] (char* msg) mutable -> void {
@@ -25,13 +32,18 @@ bool robotInit(void) {
   if(ok) {
     ClientThread::instance().sendData("D TESTER I'm new");
   }
-  return ok;
-}
 
-void robotLoop(void) {
-  return;
-}
+  while(Aversive::isRunning()) {
+    // Your while(1) code
+    Aversive::sleep(); // Very important for simulation purpose
+  }
+  
+  // You can have several "while(Aversive::isRunning())" loops if needed
+  // Keep the "Aversive::sleep()" at the end of the loop in each of your loops
+  
+  // Unintialize your stuff here
+  Aversive::setReturnCode(0); // Optional; default value is already 0
+  return Aversive::exit();
 
-void robotExit(void) {
-  return;
+  return 0;
 }
