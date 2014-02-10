51d373ce237510bc6e445e16e4b787004fd15774
ldauphin <astralien3000@yahoo.fr>
2014-02-10 17:52:42 +0100

    Stream implementation "finished". Warning ! Problems with linking.

--- include/common/device/stream/stream.hpp
+++ include/common/device/stream/stream.hpp
@@ -17,9 +17,55 @@ public:
 private:
   Mode _mode = FORMATTED;
   SepMode _sep = WORD;
-  u16 _max_size = 32;
 
-protected:
+private:
+  // Write
+  void binaryWrite(const char*, u16);
+
+  //// Aversive Integers
+  void binaryWrite(const s8&);
+  void binaryWrite(const s16&);
+  void binaryWrite(const s32&);
+  void binaryWrite(const s64&);
+
+  void binaryWrite(const u8&);
+  void binaryWrite(const u16&);
+  void binaryWrite(const u32&);
+  void binaryWrite(const u64&);
+
+  void formattedWrite(const s8&);
+  void formattedWrite(const s16&);
+  void formattedWrite(const s32&);
+  void formattedWrite(const s64&);
+
+  void formattedWrite(const u8&);
+  void formattedWrite(const u16&);
+  void formattedWrite(const u32&);
+  void formattedWrite(const u64&);
+
+  // Read
+  void binaryRead(char*, u16);
+
+  // Aversive Integers
+  void binaryRead(s8&);
+  void binaryRead(s16&);
+  void binaryRead(s32&);
+  void binaryRead(s64&);
+
+  void binaryRead(u8&);
+  void binaryRead(u16&);
+  void binaryRead(u32&);
+  void binaryRead(u64&);
+
+  void formattedRead(s8&);
+  void formattedRead(s16&);
+  void formattedRead(s32&);
+  void formattedRead(s64&);
+
+  void formattedRead(u8&);
+  void formattedRead(u16&);
+  void formattedRead(u32&);
+  void formattedRead(u64&);
 
 public:
   //! \brief Default Constructor
@@ -36,23 +82,49 @@ public:
   }
 
   //! \brief Write a string into the stream (const version)
-  Stream& operator<<(const char*);
+  inline Stream& operator<<(const char* str) {
+    binaryWrite(str, 32);
+    return *this;
+  }
 
+  /*
   //! \brief Write a string into the stream
-  Stream& operator<<(char*);
+  inline Stream& operator<<(char* str) {
+    return *this << (const char*)str;
+  }
+  */
 
   //! \brief Write anything into the stream
   template<typename T>
-  Stream& operator<<(const T&);
+  inline Stream& operator<<(const T& val) {
+    if(_mode == BINARY) {
+      binaryWrite(val);
+    }
+    else if(_mode == FORMATTED) {
+      formattedWrite(val);
+    }
+    return *this;
+  }
 
   //! \brief Read a string from the stream
   //! \attention Its behavior depends on the string mode the stream is currently in
-  Stream& operator>>(char*);
+  inline Stream& operator>>(char* str) {
+    binaryRead(str, 32);
+    return *this;
+  }
   
   //! \brief Read anything from the stream
   //! \attention Its behavior depends on the mode (binary or formatted) the stream is currently in
   template<typename T>
-  Stream& operator>>(T&);
+  inline Stream& operator>>(T& val) {
+    if(_mode == BINARY) {
+      binaryRead(val);
+    }
+    else if(_mode == FORMATTED) {
+      formattedRead(val);
+    }
+    return *this;
+  }
 
   //! \brief Write a char in stream
   //! \warning virtual operation
--- src/common/device/stream/stream.cpp
+++ src/common/device/stream/stream.cpp
@@ -1,75 +1,140 @@
 #include <device/stream/stream.hpp>
 
+const u16 MAX_BUFF = 32;
+
+////////////////////////////////////////
+// Constructor
+
 Stream::Stream(const char* name) : Device(name) {}
 
-Stream& Stream::operator<<(const char* str) {
-  for( ; *str != '\0' ; str++) {
+////////////////////////////////////////
+// Binary Write
+
+void Stream::binaryWrite(const char* str, u16 size) {  
+  const char* ptr = str;
+  for( ; *ptr != '\0' && (ptr - str) < size  ; ptr++) {
     setValue(*str);
-    //*this << *str;
   }
-  return *this;
-}
-
-
-Stream& Stream::operator<<(char* str) {
-  return *this << (const char*)str;
 }
 
 template<typename T>
-Stream& Stream::operator<<(const T& val) {
-  char str[32] = {0};
-
-  if(_mode == BINARY) {
-    for(u16 i = 0 ; i < sizeof(val) ; i++) {
-      //str[i] = (char)val >> ((sizeof(val) - 1 - i) * 8);
-      setValue(((u64)val >> (i * 8)) % 256);
-    }
+inline static void basic_binary_write(Stream& s, const T& val) {
+  for(u16 i = 0 ; i < sizeof(val) ; i++) {
+    s.setValue((val >> (i * 8)) % 256);
   }
-  else if(_mode == FORMATTED) {
-    T tmp = val;
-    
-    u16 beg = 0;
-    if(val == 0) {
-      str[beg++] = '0';
-    }
-    else if(val < 0) {
-      str[beg++] = '-';
-      tmp = -val;
-    }
-    
-    u16 i = beg;
-    while(0 < tmp && i < 32) {
-      str[i] = '0' + tmp % 10;
-      tmp /= 10;
-      i++;
-    }
-    
-    i--;
-    while(beg < i) {
-      char aux = str[i];
-      str[i] = str[beg];
-      str[beg] = aux;
-      i--;
-      beg++;
-    }
-    *this << (const char*)str;
-  }
-
-  return *this;
 }
 
-Stream& Stream::operator>>(char* str) {
+void Stream::binaryWrite(const s8& val) {
+  basic_binary_write(*this, val);
+}
+
+void Stream::binaryWrite(const s16& val) {
+  basic_binary_write(*this, val);
+}
+
+void Stream::binaryWrite(const s32& val) {
+  basic_binary_write(*this, val);
+}
+
+void Stream::binaryWrite(const s64& val) {
+  basic_binary_write(*this, val);
+}
+
+void Stream::binaryWrite(const u8& val) {
+  basic_binary_write(*this, val);
+}
+
+void Stream::binaryWrite(const u16& val) {
+  basic_binary_write(*this, val);
+}
+
+void Stream::binaryWrite(const u32& val) {
+  basic_binary_write(*this, val);
+}
+
+void Stream::binaryWrite(const u64& val) {
+  basic_binary_write(*this, val);
+}
+
+////////////////////////////////////////
+// Formatted Write
+
+template<bool SIGNED = true, typename T>
+inline static void basic_formatted_integer_write(Stream& s, T val) {  
+  char str[MAX_BUFF] = {0};
+
+  char* ptr = str + MAX_BUFF;
+  bool neg = false;
+
+  *(--ptr) = '\0';
+
+  if(SIGNED && val < 0) {
+    val = -val;
+    neg = true;
+  }
+
+  while(0 < val && str < ptr) {
+    *(--ptr) = '0' + (val % 10);
+    val /= 10;
+  }
+
+  if(SIGNED && neg) {
+    *(--ptr) = '-';
+  }
+  
+  if(ptr == str + MAX_BUFF) {
+    *(--ptr) = '0';
+  }
+  
+  s << (const char*)ptr;
+}
+
+void Stream::formattedWrite(const s8& val) {
+  basic_formatted_integer_write(*this, val);
+}
+
+void Stream::formattedWrite(const s16& val) {
+  basic_formatted_integer_write(*this, val);
+}
+
+void Stream::formattedWrite(const s32& val) {
+  basic_formatted_integer_write(*this, val);
+}
+
+void Stream::formattedWrite(const s64& val) {
+  basic_formatted_integer_write(*this, val);
+}
+
+void Stream::formattedWrite(const u8& val) {
+  basic_formatted_integer_write<false>(*this, val);
+}
+
+void Stream::formattedWrite(const u16& val) {
+  basic_formatted_integer_write<false>(*this, val);
+}
+
+void Stream::formattedWrite(const u32& val) {
+  basic_formatted_integer_write<false>(*this, val);
+}
+
+void Stream::formattedWrite(const u64& val) {
+  basic_formatted_integer_write<false>(*this, val);
+}
+
+////////////////////////////////////////
+// Binary Read
+
+void Stream::binaryRead(char* str, u16 size) {
   bool keep = true;
   char* beg = str;
-  while(keep && (str-beg) < 32) {
+  while(keep && (str-beg) < size) {
     char c = getValue();
-    //*this >> c;
     *str = c;
     if(_mode == FORMATTED) {
       if(_sep == WORD && c == ' ') {
 	keep = false;
       }
-      else if(c == '\r' || c == '\n') {
+      else if(c == '\r' || c == '\n' || c == '\0') {
 	*str = '\n';
 	keep = false;
       }
@@ -79,67 +144,105 @@ Stream& Stream::operator>>(char* str) {
     str++;	
   }
   *(str-1) = '\0';
-  return *this;
 }
 
 template<typename T>
-Stream& Stream::operator>>(T& val) {
-  val = 0;
-  
-  if(_mode == BINARY) {
-    for(u16 i = 0 ; i < sizeof(val) ; i++) {
-      //val += str[i] << (i * 8);
-      val += getValue() << (i * 8) % 256;
-    }
+inline static void basic_binary_read(Stream& s, T& val) {
+  for(u16 i = 0 ; i < sizeof(val) ; i++) {
+    val += s.getValue() << (i * 8) % 256;
   }
-  else if(_mode == FORMATTED) {
-    char str[32] = {0};
-    *this >> str;
-
-    u16 beg = 0;
-    if(str[0] == '-') {
-      beg = 1;
-    }
-    
-    for(u16 i = beg ; str[i] ; i++) {
-      if('0' <= str[i] && str[i] <= '9') {
-	val *= 10;
-	val += (T)(str[i] - '0');
-      }
-      else {
-	str[i+1] = '\0';
-      }
-    }
-
-    if(beg == 1) {
-      val = -val;
-    }
-  }
-  return *this;
 }
 
-// Input
-/*
-template Stream& Stream::operator>> <u8>(u32&);
-template Stream& Stream::operator>> <u16>(u16&);
-template Stream& Stream::operator>> <u32>(u32&);
-template Stream& Stream::operator>> <u64>(u64&);
-*/
+void Stream::binaryRead(s8& val) {
+  basic_binary_read(*this, val);
+}
 
-template Stream& Stream::operator>> <s8>(s8&);
-template Stream& Stream::operator>> <s16>(s16&);
-template Stream& Stream::operator>> <s32>(s32&);
-template Stream& Stream::operator>> <s64>(s64&);
+void Stream::binaryRead(s16& val) {
+  basic_binary_read(*this, val);
+}
 
-// Output
-/*
-template Stream& Stream::operator<< <u8>(const u8&);
-template Stream& Stream::operator<< <u16>(const u16&);
-template Stream& Stream::operator<< <u32>(const u32&);
-template Stream& Stream::operator<< <u64>(const u64&);
-*/
+void Stream::binaryRead(s32& val) {
+  basic_binary_read(*this, val);
+}
 
-template Stream& Stream::operator<< <s8>(const s8&);
-template Stream& Stream::operator<< <s16>(const s16&);
-template Stream& Stream::operator<< <s32>(const s32&);
-template Stream& Stream::operator<< <s64>(const s64&);
+void Stream::binaryRead(s64& val) {
+  basic_binary_read(*this, val);
+}
+
+void Stream::binaryRead(u8& val) {
+  basic_binary_read(*this, val);
+}
+
+void Stream::binaryRead(u16& val) {
+  basic_binary_read(*this, val);
+}
+
+void Stream::binaryRead(u32& val) {
+  basic_binary_read(*this, val);
+}
+
+void Stream::binaryRead(u64& val) {
+  basic_binary_read(*this, val);
+}
+
+////////////////////////////////////////
+// Formatted Write
+
+template<bool SIGNED = true, typename T>
+void basic_formatted_integer_read(Stream& s, T& val) {
+  val = 0;
+
+  char str[32] = {0};
+  s >> str;
+
+  u16 beg = 0;
+  if(SIGNED && str[0] == '-') {
+    beg = 1;
+  }
+    
+  for(u16 i = beg ; str[i] ; i++) {
+    if('0' <= str[i] && str[i] <= '9') {
+      val *= 10;
+      val += (T)(str[i] - '0');
+    }
+    else {
+      str[i+1] = '\0';
+    }
+  }
+
+  if(SIGNED && beg == 1) {
+    val = -val;
+  }
+}
+
+void Stream::formattedRead(s8& val) {
+  basic_formatted_integer_read(*this, val);
+}
+
+void Stream::formattedRead(s16& val) {
+  basic_formatted_integer_read(*this, val);
+}
+
+void Stream::formattedRead(s32& val) {
+  basic_formatted_integer_read(*this, val);
+}
+
+void Stream::formattedRead(s64& val) {
+  basic_formatted_integer_read(*this, val);
+}
+
+void Stream::formattedRead(u8& val) {
+  basic_formatted_integer_read<false>(*this, val);
+}
+
+void Stream::formattedRead(u16& val) {
+  basic_formatted_integer_read<false>(*this, val);
+}
+
+void Stream::formattedRead(u32& val) {
+  basic_formatted_integer_read<false>(*this, val);
+}
+
+void Stream::formattedRead(u64& val) {
+  basic_formatted_integer_read<false>(*this, val);
+}
