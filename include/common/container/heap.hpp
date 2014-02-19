bee0e8ab86a32d099979788cee7e51606b00e912
ldauphin <astralien3000@yahoo.fr>
2014-02-19 22:41:59 +0100

    Imporving heap. Updating class diagram.

--- include/common/container/heap.hpp
+++ include/common/container/heap.hpp
@@ -17,42 +17,124 @@
 template<int S, typename T>
 class Heap {
 private:
+  friend class Node;
+
   Array<S, T> _data;
   int _size;
 
-  void percolateUp(void) {
-    int i = _size - 1;
-    while(0 < i && _data[(i-1)/2] < _data[i]) {
-      T aux = _data[i];
-      _data[i] = _data[(i-1)/2];
-      _data[(i-1)/2] = aux;
-      i = (i-1) / 2;
+  //! \brief Private class representing a node in a binary tree
+  /*
+    Useful for percolation functions.
+   */
+  class Node {
+  private:
+    Heap& _heap;
+    int _index;
+
+  public:
+    //! \brief Constructor
+    inline Node(Heap& h, int index) : _heap(h), _index(index) {}
+    
+    //! \brief Copy operator
+    inline Node& operator=(const Node& other) {
+      _heap = other._heap;
+      _index = other._index;
+      return *this;
+    }
+    
+    //! \brief Check if the node really represents an element is the heap
+    inline bool exist(void) {
+      return 0 < _index && _index < _heap._size;
+    }
+    
+    //! \brief Exchange elements of two nodes
+    //! \warning Does not change the node's position.
+    inline void switchElements(Node other) {
+      T aux = element();
+      element() = other.element();
+      other.element() = aux;
+    }
+    
+    //! \brief Returns a reference to the node's parent
+    inline Node parent(void) {
+      return Node(_heap, (_index-1)/2);
+    }
+
+    //! \brief Returns a reference to the node's first child
+    inline Node firstChild(void) {
+      return Node(_heap, (_index*2)+1);
+    }
+
+    //! \brief Returns a reference to the node's second child
+    inline Node secondChild(void) {
+      return Node(_heap, (_index*2)+2);
+    }
+
+    //! \brief Check if the node is not the root
+    //! (faster than parent.exist)
+    inline bool parentExist(void) {
+      return 0 < parent()._index;
+    }
+
+    //! \brief Check if the node has a first child 
+    //! (faster than firstChild.exist)
+    inline bool firstChildExist(void) {
+      return firstChild()._index < _heap._size;
+    }
+
+    //! \brief Check if the node has a second child 
+    //! (faster than secondChild.exist)
+    inline bool secondChildExist(void) {
+      return secondChild()._index < _heap._size;
+    }
+
+    //! \brief Returns a reference to the element of the node
+    inline T& element(void) {
+      return _heap._data[_index];
+    }
+  };
+
+  //! \brief Place correctly an element which have to move upward
+  void percolateUp(int index) {
+    Node cur(*this, index);
+    if(cur.exist()) {
+      while(cur.parentExist() && cur.parent().element() < cur.element()) {
+	cur.switchElements(cur.parent());
+	cur = cur.parent();
+      }
     }
   }
 
-  void percolateDown(void) {
-    int i = 0;
-    while((i * 2 + 1 < _size && _data[i] < _data[i * 2 + 1]) || 
-	  (i * 2 + 2 < _size && _data[i] < _data[i * 2 + 2])) {
-      T aux = _data[i];
-      if(i * 2 + 1 >= _size) {
-	_data[i] = _data[i * 2 + 2];
-	_data[i * 2 + 2] = aux;
-	i = i * 2 + 2;
-      }
-      else if(i * 2 + 2 >= _size) {
-	_data[i] = _data[i * 2 + 1];
-	_data[i * 2 + 1] = aux;
-	i = i * 2 + 1;
-      }
-      else {
-	_data[i] = Math::max(_data[i * 2 + 2], _data[i * 2 + 1]);
-	Math::max(_data[i * 2 + 2], _data[i * 2 + 1]) = aux;
-	if(_data[i * 2 + 2] == aux) {
-	  i = i * 2 + 2;
+  //! \brief Place correctly an element which have to move downward
+  void percolateDown(int index) {
+    Node cur(*this, index);
+    if(cur.exist()) {
+      // While there is one child greater than current
+      while((cur.firstChildExist() && 
+	     cur.element() < cur.firstChild().element()) ||
+	    (cur.secondChildExist() && 
+	     cur.element() < cur.secondChild().element())) {
+	// If there is only one child
+	// The first child must be filled first !
+	if(!cur.firstChildExist()) {
+	  cur.switchElements(cur.secondChild());
+	  cur = cur.secondChild();
 	}
+	else if(!cur.secondChildExist()) {
+	  cur.switchElements(cur.firstChild());
+	  cur = cur.firstChild();
+	}
+	// If there is two children
 	else {
-	  i = i * 2 + 1;
+	  // Switch with the max
+	  if(cur.firstChild().element() < cur.secondChild().element()) {
+	    cur.switchElements(cur.secondChild());
+	    cur = cur.secondChild();
+	  }
+	  else {
+	    cur.switchElements(cur.firstChild());
+	    cur = cur.firstChild();
+	  }
 	}
       }
     }
@@ -80,15 +162,14 @@ public:
   //! \brief Insert an element in the Heap
   void insert(const T& e) {
     _data[_size] = e;
-    _size++;
-    percolateUp();
+    percolateUp(_size++);
   }
 
   //! \brief Delete the maximal element
   void pop(void) {
     _data[0] = _data[_size - 1];
     _size--;
-    percolateDown();
+    percolateDown(0);
   }
 
   //! \brief Access to the maximal element
@@ -117,15 +198,15 @@ public:
     return _size == S;
   }
   
-  //! \brief Return the amount of free space
-  int freeSpace(void) const {
-    return S - _size;
-  }
+  // //! \brief Return the amount of free space
+  // int freeSpace(void) const {
+  //   return S - _size;
+  // }
   
-  //! \brief Return the amount of used space
-  int usedSpace(void) const {
-    return _size;
-  }
+  // //! \brief Return the amount of used space
+  // int usedSpace(void) const {
+  //   return _size;
+  // }
   
   //! \brief To execute an operation on all elements contained
   void doForeach(void (*func)(T&)) {
