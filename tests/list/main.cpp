d923304f9c0987dc74266ef9c1ec34b4f1843204
Elioty <roadkiller.cl@gmail.com>
2014-03-09 19:03:41 +0100

    Updating List to make it comply with the lastest coding conventions. Updating List test.

--- tests/list/main.cpp
+++ tests/list/main.cpp
@@ -1,44 +1,33 @@
 #include <container/list.hpp>
 #include <iostream>
-using namespace std;
+#include <cassert>
 
-template<int S, typename E>
-ostream& operator<<(ostream& out, List<S, E>& list) {
-  out << "( ";
-  list.doForeach([&out](const E& e) {
-      out << e << " ";
-    });
-  return out << ")";
-}
+static const list_t SIZE = 10;
 
-int main(int argc, char* argv[]) {
-  List<10, int> l1(10,5,8,20);
-  List<10, int> l2(5,3,1,6,7,12,16,44,66,42);
-  List<10, int> l3 = l1;
-
-  cout << "l1 " << l1 << endl;
-  cout << "l2 " << l2 << endl;
-  cout << "l3 " << l3 << endl;
-
-  l3.insert(0, l2.get(0));
-  cout << "l3 " << l3 << endl;
-
-  l3.remove(2);
-  cout << "l3 " << l3 << endl;
+int main(int argc, char** argv) {
+  (void) argc;
+  (void) argv;
   
-  cout << "Full :" << endl;
-  l2.insert(5, 666); // Out of capacity
-  cout << "l2 " << l2 << endl;
-
-  cout << "==" << endl;
-  l2.remove(0);
-  cout << "l2 " << l2 << endl;
-
-  cout << "Invalid index :" << endl; 
-  l2.insert(9, 666); // Out of size
-  cout << "l2 " << l2 << endl;
-
-  l2.insert(-1, 666); // Out of size
-  cout << "l2 " << l2 << endl;
+  List<SIZE, u8> l(10,5,8,20);
+  
+  assert(l.get(0) == 10);
+  assert(l.get(1) == 5);
+  assert(l.get(3) == 20);
+  assert(l.usedSpace() == 4);
+  assert(l.freeSpace() == (SIZE) - l.usedSpace());
+  assert(l.append(12));
+  assert(l.get(4) == 12);
+  assert(l.usedSpace() == 5);
+  assert(l.prepend(2));
+  assert(l.get(0) == 2);
+  assert(l.get(5) == 12);
+  assert(!l.isFull());
+  assert(!l.isEmpty());
+  assert(l.usedSpace() == 6);
+  assert(l.indexOf(10) == 1);
+  assert(l.contains(20));
+  assert(!l.contains(100));
+  
+  std::cout << "OK" << std::endl;
   return 0;
 }
