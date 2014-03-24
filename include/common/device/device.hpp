4456523be7a3ec8508a80a812f07f0d5a011a781
Elioty <roadkiller.cl@gmail.com>
2014-03-25 00:50:27 +0100

    Cleaning up and documenting Device.

--- include/common/device/device.hpp
+++ include/common/device/device.hpp
@@ -1,15 +1,34 @@
 #ifndef DEVICE_HPP
 #define DEVICE_HPP
 
+//! \class Device device.hpp <device/device.hpp>
+//! \brief Class representing a generic device.
 class Device {
 private:
+  //! \brief The device's name.
   const char* _name;
-
-public:
-  //! \brief Create a device with a name, used by sasiae to identify the device
-  explicit inline Device(const char* name) : _name(name) {}
   
-  //! \brief Returns the name of the device
+  //! \brief Private default constructor to prevent its use.
+  inline Device(void) {
+  }
+  
+protected:
+  //! \brief Protected copy constructor to prevent its use by final user.
+  //! \param other : the device to copy.
+  inline Device(const Device& other)
+    : _name(other._name) {
+  }
+  
+public:
+  //! \brief Construct a device with a given name.
+  //! \param name : the device's name.
+  //! \note The device's name is used by SASIAE to identify it.
+  explicit inline Device(const char* name)
+    : _name(name) {
+  }
+  
+  //! \brief Return the name of the device.
+  //! \return The device's name.
   inline const char* name(void) const {
     return _name;
   }
