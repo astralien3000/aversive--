bbb8c78197f4e689767e2f9872a3d986aa0e722e
Elioty <roadkiller.cl@gmail.com>
2014-03-25 01:03:42 +0100

    Documenting Input and Output.

--- include/common/device/input.hpp
+++ include/common/device/input.hpp
@@ -1,10 +1,14 @@
 #ifndef INPUT_HPP
 #define INPUT_HPP
 
-//! \brief Input interface, used for sensors
+//! \class Input input.hpp <device/input.hpp>
+//! \brief Input interface used by physical and virtual input devices.
+//! \param T : the type of the data read from the input.
 template<typename T>
 class Input {
 public:
+  //! \brief Return the current value of the input.
+  //! \return The current value of the input.
   virtual T getValue(void) = 0;
 };
 
--- include/common/device/output.hpp
+++ include/common/device/output.hpp
@@ -1,11 +1,16 @@
 #ifndef OUTPUT_HPP
 #define OUTPUT_HPP
 
-//! \brief Output interface, used for actuators
+//! \class Output output.hpp <device/output.hpp>
+//! \brief Output interface used by physical and virtual output devices.
+//! \param T : the type of the data written into the output.
 template<typename T>
 class Output {
 public:
-  virtual void setValue(T) = 0;
+  //! \brief Set the value of the output to a new value.
+  //! \param value : the new value of the output.
+  //! \todo Turn the parameter into a const reference (and then update all child classes + re-run tests)
+  virtual void setValue(T value) = 0;
 };
 
 #endif//OUTPUT_HPP
