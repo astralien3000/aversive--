42c462e5a362f93d6b4a13e3bd05df038ddd4f63
Elioty <roadkiller.cl@gmail.com>
2014-03-25 02:04:25 +0100

    Cleaning and documenting Stream. Adding a todo for renaming Stream::SepMode into Stream::Separator (or Stream::StringSeparator).

--- include/common/device/stream/stream.hpp
+++ include/common/device/stream/stream.hpp
@@ -5,101 +5,236 @@
 #include <device/output.hpp>
 #include <base/integer.hpp>
 
+//! \class Stream stream.hpp <device/stream/stream.hpp>
+//! \brief Abstract two-way stream with formatted and binary read and write.
+//! \todo Change SepMode into Separator (or StringSeparator).
 class Stream : public Input<char>, public Output<char> {
 public:
-  //! \brief Mode represents the different modes the stream can be in (default is FORMATTED)
-  enum Mode { BINARY, FORMATTED };
+  //! \brief Represents the different ways a stream can read and write data.
+  //! \details Defaut is formatted.
+  enum Mode {
+    BINARY,   //!< Read and write data as binary.
+    FORMATTED //!< Read and write formatted data.
+  };
   
-  //! \brief StrMode represents the different modes the stream can read string (default is WORD)
-  enum SepMode { WORD, LINE };
+  //! \brief Represents the different ways a stream can read string.
+  //! \details Word separators are space and \\t.
+  //! \details Line separators are \\n, \\r and \\0.
+  //! \details Default is word.
+  //! \note If the stream is in binary mode, whatever the string separator might be, a string reading will completely fulfil the user's buffer.
+  enum SepMode {
+    WORD, //!< Read string word by word.
+    LINE  //!< Read string line by line.
+  };
   
-  //! \brief Default maximum length of string read from the stream and copied into the user's buffer
+  //! \brief Default maximum length of string read from the stream and copied into the user's buffer.
   static const u8 DEFAULT_USER_BUFFER_SIZE = 32;
   
 private:
+  //! \brief The current mode of the stream.
   Mode _mode = FORMATTED;
+  
+  //! \brief The current way the stream read string.
   SepMode _sep = WORD;
+  
+  //! \brief User's buffer size.
   u8 _user_buffer_size = DEFAULT_USER_BUFFER_SIZE;
-
-private:
-  // Write
-  void binaryWrite(const char*);
-
-  //// Aversive Integers
-  void binaryWrite(const s8&);
-  void binaryWrite(const s16&);
-  void binaryWrite(const s32&);
-  void binaryWrite(const s64&);
-
-  void binaryWrite(const u8&);
-  void binaryWrite(const u16&);
-  void binaryWrite(const u32&);
-  void binaryWrite(const u64&);
-
-  void formattedWrite(const s8&);
-  void formattedWrite(const s16&);
-  void formattedWrite(const s32&);
-  void formattedWrite(const s64&);
-
-  void formattedWrite(const u8&);
-  void formattedWrite(const u16&);
-  void formattedWrite(const u32&);
-  void formattedWrite(const u64&);
-
-  // Read
-  void binaryRead(char*, u16);
-
-  // Aversive Integers
-  void binaryRead(s8&);
-  void binaryRead(s16&);
-  void binaryRead(s32&);
-  void binaryRead(s64&);
-
-  void binaryRead(u8&);
-  void binaryRead(u16&);
-  void binaryRead(u32&);
-  void binaryRead(u64&);
-
-  void formattedRead(s8&);
-  void formattedRead(s16&);
-  void formattedRead(s32&);
-  void formattedRead(s64&);
-
-  void formattedRead(u8&);
-  void formattedRead(u16&);
-  void formattedRead(u32&);
-  void formattedRead(u64&);
-
+  
+  //! \brief Write a string into the stream.
+  //! \param str : the string to write.
+  void binaryWrite(const char* str);
+  
+  //! \brief Write an integer as binary into the stream.
+  //! \param i : the integer to write.
+  void binaryWrite(const s8& i);
+  
+  //! \brief Write an integer as binary into the stream.
+  //! \param i : the integer to write.
+  void binaryWrite(const s16& i);
+  
+  //! \brief Write an integer as binary into the stream.
+  //! \param i : the integer to write.
+  void binaryWrite(const s32& i);
+  
+  //! \brief Write an integer as binary into the stream.
+  //! \param i : the integer to write.
+  void binaryWrite(const s64& i);
+  
+  //! \brief Write an integer as binary into the stream.
+  //! \param i : the integer to write.
+  void binaryWrite(const u8& i);
+  
+  //! \brief Write an integer as binary into the stream.
+  //! \param i : the integer to write.
+  void binaryWrite(const u16& i);
+  
+  //! \brief Write an integer as binary into the stream.
+  //! \param i : the integer to write.
+  void binaryWrite(const u32& i);
+  
+  //! \brief Write an integer as binary into the stream.
+  //! \param i : the integer to write.
+  void binaryWrite(const u64& i);
+  
+  //! \brief Write a formatted integer into the stream.
+  //! \param i : the integer to write.
+  void formattedWrite(const s8& i);
+  
+  //! \brief Write a formatted integer into the stream.
+  //! \param i : the integer to write.
+  void formattedWrite(const s16& i);
+  
+  //! \brief Write a formatted integer into the stream.
+  //! \param i : the integer to write.
+  void formattedWrite(const s32& i);
+  
+  //! \brief Write a formatted integer into the stream.
+  //! \param i : the integer to write.
+  void formattedWrite(const s64& i);
+  
+  //! \brief Write a formatted integer into the stream.
+  //! \param i : the integer to write.
+  void formattedWrite(const u8& i);
+  
+  //! \brief Write a formatted integer into the stream.
+  //! \param i : the integer to write.
+  void formattedWrite(const u16& i);
+  
+  //! \brief Write a formatted integer into the stream.
+  //! \param i : the integer to write.
+  void formattedWrite(const u32& i);
+  
+  //! \brief Write a formatted integer into the stream.
+  //! \param i : the integer to write.
+  void formattedWrite(const u64& i);
+  
+  //! \brief Read a string from the stream.
+  //! \param str : the buffer to write the read string into.
+  //! \param len : the maximum amount of characters to write into the given buffer.
+  void binaryRead(char* str, u16 len);
+  
+  //! \brief Read an integer as binary from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void binaryRead(s8& i);
+  
+  //! \brief Read an integer as binary from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void binaryRead(s16& i);
+  
+  //! \brief Read an integer as binary from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void binaryRead(s32& i);
+  
+  //! \brief Read an integer as binary from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void binaryRead(s64& i);
+  
+  //! \brief Read an integer as binary from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void binaryRead(u8& i);
+  
+  //! \brief Read an integer as binary from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void binaryRead(u16& i);
+  
+  //! \brief Read an integer as binary from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void binaryRead(u32& i);
+  
+  //! \brief Read an integer as binary from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void binaryRead(u64& i);
+  
+  //! \brief Read a formatted integer from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void formattedRead(s8& i);
+  
+  //! \brief Read a formatted integer from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void formattedRead(s16& i);
+  
+  //! \brief Read a formatted integer from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void formattedRead(s32& i);
+  
+  //! \brief Read a formatted integer from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void formattedRead(s64& i);
+  
+  //! \brief Read a formatted integer from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void formattedRead(u8& i);
+  
+  //! \brief Read a formatted integer from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void formattedRead(u16& i);
+  
+  //! \brief Read a formatted integer from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void formattedRead(u32& i);
+  
+  //! \brief Read a formatted integer from the stream.
+  //! \param i : a reference to the integer to overwrite.
+  void formattedRead(u64& i);
+  
 protected:
+  //! \brief A boolean telling whether the minibuffer is used or not.
   bool _mini_buffer_used;
   
+  //! \brief The mini buffer used by nextValue method.
   char _mini_buffer;
   
 public:
-  //! \brief Default Constructor
+  //! \brief Default constructor.
   inline Stream(void)
     : _mini_buffer_used(false) {
   }
-
-  //! \brief Change the mode the stream is currently in
+  
+  //! \brief Copy constructor.
+  //! \param s : the stream to copy.
+  inline Stream(const Stream& s)
+    : _mini_buffer_used(s._mini_buffer_used), _mini_buffer(s._mini_buffer) {
+  }
+  
+  //! \brief Return the mode the stream is currently in.
+  //! \return The current stream's mode.
+  inline Mode mode(void) const {
+    return _mode;
+  }
+  
+  //! \brief Change the mode the stream is currently in.
+  //! \param m : the new mode.
   inline void setMode(Mode m) {
     _mode = m;
   }
-
-  //! \brief Change the string mode the stream is currently in
+  
+  //! \brief Return the string separator the stream currently uses.
+  //! \return The current stream's string separator.
+  inline SepMode sepMode(void) const {
+    return _sep;
+  }
+  
+  //! \brief Change the way the stream read strings.
+  //! \param s : the new string separtor.
   inline void setSepMode(SepMode s) {
     _sep = s;
   }
   
+  //! \brief Return the current user's buffer size used by the stream.
+  //! \return The user's buffer size.
+  inline u8 userBufferSize(void) const {
+    return _user_buffer_size;
+  }
+  
+  //! \brief Change the user's buffer size.
+  //! \param s : the new user's buffer size.
   inline void setUserBufferSize(u8 s) {
     _user_buffer_size = s;
   }
   
-  inline u8 getUserBufferSize(void) {
-    return _user_buffer_size;
-  }
-  
-  //! \brief Write a signed integer into the stream
+  //! \brief Write an integer into the stream.
+  //! \param val : the integer to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const s8& val) {
     if(_mode == BINARY) {
       binaryWrite(val);
@@ -110,7 +245,9 @@ public:
     return *this;
   }
   
-  //! \brief Write a signed integer into the stream
+  //! \brief Write an integer into the stream.
+  //! \param val : the integer to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const s16& val) {
     if(_mode == BINARY) {
       binaryWrite(val);
@@ -121,7 +258,9 @@ public:
     return *this;
   }
   
-  //! \brief Write a signed integer into the stream
+  //! \brief Write an integer into the stream.
+  //! \param val : the integer to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const s32& val) {
     if(_mode == BINARY) {
       binaryWrite(val);
@@ -132,7 +271,9 @@ public:
     return *this;
   }
   
-  //! \brief Write a signed integer into the stream
+  //! \brief Write an integer into the stream.
+  //! \param val : the integer to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const s64& val) {
     if(_mode == BINARY) {
       binaryWrite(val);
@@ -143,7 +284,9 @@ public:
     return *this;
   }
   
-  //! \brief Write an unsigned integer into the stream
+  //! \brief Write an integer into the stream.
+  //! \param val : the integer to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const u8& val) {
     if(_mode == BINARY) {
       binaryWrite(val);
@@ -154,7 +297,9 @@ public:
     return *this;
   }
   
-  //! \brief Write an unsigned integer into the stream
+  //! \brief Write an integer into the stream.
+  //! \param val : the integer to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const u16& val) {
     if(_mode == BINARY) {
       binaryWrite(val);
@@ -165,7 +310,9 @@ public:
     return *this;
   }
   
-  //! \brief Write an unsigned integer into the stream
+  //! \brief Write an integer into the stream.
+  //! \param val : the integer to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const u32& val) {
     if(_mode == BINARY) {
       binaryWrite(val);
@@ -176,7 +323,9 @@ public:
     return *this;
   }
   
-  //! \brief Write an unsigned integer into the stream
+  //! \brief Write an integer into the stream.
+  //! \param val : the integer to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const u64& val) {
     if(_mode == BINARY) {
       binaryWrite(val);
@@ -187,13 +336,17 @@ public:
     return *this;
   }
   
-  //! \brief Write a string into the stream
+  //! \brief Write a string into the stream.
+  //! \param str : the string to write.
+  //! \return A reference to the stream.
   inline Stream& operator<<(const char* str) {
     binaryWrite(str);
     return *this;
   }
   
-  //! \brief Read a signed integer from the stream
+  //! \brief Read an integer from the stream.
+  //! \param val : a reference to the integer to overwrite.
+  //! \return A reference to the stream.
   inline Stream& operator>>(s8& val) {
     if(_mode == BINARY) {
       binaryRead(val);
@@ -204,7 +357,9 @@ public:
     return *this;
   }
   
-  //! \brief Read a signed integer from the stream
+  //! \brief Read an integer from the stream.
+  //! \param val : a reference to the integer to overwrite.
+  //! \return A reference to the stream.
   inline Stream& operator>>(s16& val) {
     if(_mode == BINARY) {
       binaryRead(val);
@@ -215,7 +370,9 @@ public:
     return *this;
   }
   
-  //! \brief Read a signed integer from the stream
+  //! \brief Read an integer from the stream.
+  //! \param val : a reference to the integer to overwrite.
+  //! \return A reference to the stream.
   inline Stream& operator>>(s32& val) {
     if(_mode == BINARY) {
       binaryRead(val);
@@ -226,7 +383,9 @@ public:
     return *this;
   }
   
-  //! \brief Read a signed integer from the stream
+  //! \brief Read an integer from the stream.
+  //! \param val : a reference to the integer to overwrite.
+  //! \return A reference to the stream.
   inline Stream& operator>>(s64& val) {
     if(_mode == BINARY) {
       binaryRead(val);
@@ -237,7 +396,9 @@ public:
     return *this;
   }
   
-  //! \brief Read an unsigned integer from the stream
+  //! \brief Read an integer from the stream.
+  //! \param val : a reference to the integer to overwrite.
+  //! \return A reference to the stream.
   inline Stream& operator>>(u8& val) {
     if(_mode == BINARY) {
       binaryRead(val);
@@ -248,7 +409,9 @@ public:
     return *this;
   }
   
-  //! \brief Read an unsigned integer from the stream
+  //! \brief Read an integer from the stream.
+  //! \param val : a reference to the integer to overwrite.
+  //! \return A reference to the stream.
   inline Stream& operator>>(u16& val) {
     if(_mode == BINARY) {
       binaryRead(val);
@@ -259,7 +422,9 @@ public:
     return *this;
   }
   
-  //! \brief Read an unsigned integer from the stream
+  //! \brief Read an integer from the stream.
+  //! \param val : a reference to the integer to overwrite.
+  //! \return A reference to the stream.
   inline Stream& operator>>(u32& val) {
     if(_mode == BINARY) {
       binaryRead(val);
@@ -270,7 +435,9 @@ public:
     return *this;
   }
   
-  //! \brief Read an unsigned integer from the stream
+  //! \brief Read an integer from the stream.
+  //! \param val : a reference to the integer to overwrite.
+  //! \return A reference to the stream.
   inline Stream& operator>>(u64& val) {
     if(_mode == BINARY) {
       binaryRead(val);
@@ -281,17 +448,20 @@ public:
     return *this;
   }
   
-  //! \brief Read a string from the stream
+  //! \brief Read a string from the stream.
+  //! \param str : the buffer to write the read string into.
+  //! \return A reference to the stream.
   inline Stream& operator>>(char* str) {
     binaryRead(str, _user_buffer_size);
     return *this;
   }
   
-  //! \brief Write a char in stream
-  //! \warning virtual operation
+  //! \brief Read a char from the stream.
+  //! \return The read char.
   virtual char getValue(void) = 0;
-
+  
   //! \brief Return the next value to read from the stream without removing it from the head of the stream.
+  //! \return The char at the head of the stream.
   inline char nextValue(void) {
     if(!_mini_buffer_used) {
       _mini_buffer = getValue();
@@ -299,10 +469,10 @@ public:
     }
     return _mini_buffer;
   }
-
-  //! \brief Read a char from stream
-  //! \warning virtual operation
-  virtual void setValue(char) = 0;
+  
+  //! \brief Write a char into the stream.
+  //! \param c : the character to write.
+  virtual void setValue(char c) = 0;
 };
 
 #endif//STREAM_HPP
