13e66005065dd342eb45f8ed4e2e053d309a4a17
astralien3000 <astralien3000@yahoo.fr>
2014-03-26 20:08:05 +0100

    Added AX12. Not tested. Not even implemented...

--- include/common/device/eirbot2014/ax12.hpp
+++ include/common/device/eirbot2014/ax12.hpp
@@ -1,25 +1,47 @@
 #ifndef AX12_HPP
 #define AX12_HPP
 
-#include <device/device.hpp>
+#include <device/input.hpp>
 #include <device/output.hpp>
+#include <device/device.hpp>
+
 #include <base/integer.hpp>
 
 //! \brief A digital servomotor, used by Eirbot in 2014
-//! \param ADDR : the address where to set the motor pwm
-/*! 
+/*!
 
-	This actuator is very specific because he works as an extern card
-	so you can communicate with him. It is based on UART communication
-	that's why you should read UartStream instructions.
+This actuator is very specific because he works as an extern card
+so you can communicate with him. It is based on UART communication
+that's why you should read UartStream instructions.
 
 */
-template<s32* ADDR>
-class Motor : public Device, public Output<s32> {
-public:
-  Motor(const char*);
+class AX12: public Device, Input<u16>, Output<u16> {
+private:
+  AX12PrivateData data;
 
-  void setValue(s32);
+public:
+  enum WorkingMode{WHEEL,JOIN};
+  enum ComMode{POSITION,SPEED,TORQUE};
+
+public:
+  AX12(char*);
+  ~AX12();
+
+  u16 getValue();
+  void setValue(u16);
+
+  u16 getAngle();
+  u16 getSpeed();
+  u16 getTorque();
+
+  void setAngle(u16);
+  void setSpeed(u16);
+
+  void setRecvMode(ComMode);
+  void setSendMode(WorkingMode);
+
+  void setAngleMax(u16, u16);
+  void setTorqueMax(u16);
 };
 
 #endif//AX12_HPP
--- include/sasiae/device/eirbot2014/ax12.hpp
+++ include/sasiae/device/eirbot2014/ax12.hpp
@@ -1,8 +1,20 @@
 #ifndef SASIAE_AX12_HPP
 #define SASIAE_AX12_HPP
 
+#include <base/integer.hpp>
+#include <QStringList>
+#include <QString>
+#include <client_thread.hpp>
+#include <sstream>
+
+struct AX12PrivateData{
+  u16 angle;
+  u16 speed;
+  u16 torque;
+  WorkingMode wm;
+  ComMode cm;
+};
+
 #include "../../../common/device/eirbot2014/ax12.hpp"
-
-
 
 #endif//SASIAE_AX12_HPP
