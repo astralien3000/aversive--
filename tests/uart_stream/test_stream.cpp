319ea41326c6ae7197f2b409b8047ca30d7aeeab
astralien3000 <astralien3000@yahoo.fr>
2014-04-03 16:42:59 +0200

    Modified some test for uart...

--- tests/uart_stream/test_stream.cpp
+++ tests/uart_stream/test_stream.cpp
@@ -1,6 +1,5 @@
 #include <device/stream/uart_stream.hpp>
 #include <hardware/interrupts.hpp>
-#include <hardware/eeprom.hpp>
 
 #define F_CPU 16000000L
 #include <util/delay.h>
@@ -8,18 +7,13 @@
 #include <avr/io.h>
 
 int main(void) {
-  UartStream<0> uart;
+  UartStream<0> uart(0);
 
   _delay_ms(100);
   s8 i;
   while(1) {
-    i = Eeprom::instance().read(0);
-    uart << "Reading : " << i << " in EEPROM\n";
-
     uart >> i;
-    uart << "Writing : " << i << " in EEPROM\n";
-
-    Eeprom::instance().write(0, i);
+    uart << "Read : " << i << " !\n";
   }
 
   return 0;
