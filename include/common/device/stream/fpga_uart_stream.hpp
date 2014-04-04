377d87e51e91e413eb40219cc3ee008d72c09cc2
Elioty <roadkiller.cl@gmail.com>
2014-04-04 13:22:06 +0200

    Documenting FpgaUartStream.

--- include/common/device/stream/fpga_uart_stream.hpp
+++ include/common/device/stream/fpga_uart_stream.hpp
@@ -5,23 +5,49 @@
 #include <device/device.hpp>
 #include <base/integer.hpp>
 
+//! \class FpgaUartStream fpga_uart_stream.hpp <device/stream/fpga_uart__stream.hpp>
+//! \brief Uart stream through EIRBOT's FPGA.
 class FpgaUartStream : public Device, public BufferedStream {
 private:
   FpgaUartStreamPrivateData _data;
   
 public:
+  //! \brief FpgaUartStream constructor.
+  //! \param name : the device's name.
+  //! \param tx : a reference to the variable to write in to write in the stream.
+  //! \param tx_occup : a reference to the variable telling how many characters are waiting in the FPGA's output buffer.
+  //! \param rx : a reference to the variable to read to read from the stream.
+  //! \param rx_ava : a reference to the variable telling how many characters are waiting in the FPGA's input buffer.
   inline FpgaUartStream(const char* name, u8& tx, u8& tx_occup, u8& rx, u8& rx_ava);
   
+  //! \brief FpgaUartStream constructor.
+  //! \param name : the device's name.
+  //! \param tx : a reference to the variable to write in to write in the stream.
+  //! \param tx_occup : a reference to the variable telling how many characters are waiting in the FPGA's output buffer.
+  //! \param rx : a reference to the variable to read to read from the stream.
+  //! \param rx_ava : a reference to the variable telling how many characters are waiting in the FPGA's input buffer.
   inline FpgaUartStream(const char* name, volatile u8& tx, volatile u8& tx_occup, volatile u8& rx, volatile u8& rx_ava);
   
+  //! \brief Read a char from the stream's input buffer.
+  //! \return The read character.
+  //! \warning Blocking operation if the stream's input buffer is empty.
   inline char getValue(void);
   
-  inline void setValue(char val);
+  //! \brief Write a char in the stream's input buffer.
+  //! \param c : the character to write.
+  //! \warning Blocking operation if the stream's output buffer is full
+  inline void setValue(char c);
   
+  //! \brief Return the buffers' size.
+  //! \return The buffers' size.
   inline u16 bufferSize(void) const;
   
+  //! \brief Return the amout of characters waiting to be sent into the output buffer.
+  //! \return The amout of characters within the output buffer.
   inline u16 outputFreeSpace(void) const;
   
+  //! \brief Return the amount of characters currently pending into the input buffer.
+  //! \return The amout of characters within the input buffer.
   inline u16 inputUsedSpace(void) const;
 };
 
