9b2a00d88b8f72a0bc6d50559893e8ad2fca6e04
astralien3000 <astralien3000@yahoo.fr>
2014-05-02 17:53:21 +0200

    Uart debug for atmega32

--- src/avr/hardware/uart.cpp
+++ src/avr/hardware/uart.cpp
@@ -6,7 +6,21 @@ template<int ID> Uart<ID>::Uart() {}
 
 template Uart<0>::Uart();
 
+#if defined (__AVR_ATmega128__)
 MACRO_INTERRUPT_BIND(Uart<0>, recvEvent, USART0_RX_vect)
 MACRO_INTERRUPT_BIND(Uart<0>, sendEvent, USART0_TX_vect)
-
 MACRO_INTERRUPT_BIND(Uart<0>, emptyEvent, USART0_UDRE_vect)
+#endif
+
+#if defined (__AVR_ATmega2560__)
+MACRO_INTERRUPT_BIND(Uart<0>, recvEvent, USART0_RX_vect)
+MACRO_INTERRUPT_BIND(Uart<0>, sendEvent, USART0_TX_vect)
+MACRO_INTERRUPT_BIND(Uart<0>, emptyEvent, USART0_UDRE_vect)
+#endif
+
+#if defined (__AVR_ATmega32__)
+MACRO_INTERRUPT_BIND(Uart<0>, recvEvent, USART_RXC_vect)
+MACRO_INTERRUPT_BIND(Uart<0>, sendEvent, USART_TXC_vect)
+MACRO_INTERRUPT_BIND(Uart<0>, emptyEvent, USART_UDRE_vect)
+#endif
+
