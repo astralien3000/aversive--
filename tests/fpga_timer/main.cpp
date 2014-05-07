eda409c75e06da34c2ce2e990964aac35bcab6b8
astralien3000 <astralien3000@yahoo.fr>
2014-05-07 18:28:36 +0200

    Added FpgaTimer test (sasiae/stdlib).

---
+++ tests/fpga_timer/main.cpp
@@ -0,0 +1,31 @@
+// Minimal main code
+
+#include <aversive.hpp>
+
+#include <device/eirbot2014/fpga_timer.hpp>
+
+#include <iostream>
+using namespace std;
+
+u16 S = 10, MS = 100, US = 222;
+
+int main(int argc, char** argv) {
+  (void) argc;
+  (void) argv;
+  
+  Aversive::init();
+
+  FpgaTimer timer("timer", &S, &MS, &US);
+  
+  cout << timer.getValue() << endl;
+  timer.setMode(FpgaTimer::TIME_MS);
+  cout << timer.getValue() << endl;
+  timer.setMode(FpgaTimer::TIME_US);
+  cout << timer.getValue() << endl;
+
+  while(Aversive::sync()) {
+  }
+  
+  Aversive::setReturnCode(0);
+  return Aversive::exit();
+}
