c9dc898c132e1d3ab9b823eb06ab6710ada1e668
Elioty <roadkiller.cl@gmail.com>
2014-05-08 18:41:18 +0200

    Cleaning and documenting function.hpp. Updating Function's test due to latest changes.

--- include/common/base/function.hpp
+++ include/common/base/function.hpp
@@ -4,10 +4,54 @@
 #include <base/integer.hpp>
 #include <base/alloc.hpp>
 
+//! \class Function function.hpp <base/function.hpp>
+//! \brief Class representing a generic function (works with both standard function and lambda).
+//! \param T : the function to represent prototype, eg: void(void).
 template<typename T>
-class Function;
+class Function {
+public:
+  //! \brief Default constructor.
+  inline Function(void);
+  
+  //! \brief Copy constructor.
+  //! \param other : The function to copy.
+  inline Function(const Function& other);
+  
+  //! \brief Function constructor.
+  //! \param func : The function to represent.
+  template<typename Callable>
+  inline Function(Callable func);
+  
+  //! \brief Destructor
+  inline ~Function(void);
+  
+  //! \brief Copy operator.
+  //! \param other : The function to copy.
+  Function& operator=(const Function& other);
+  
+  //! \brief Function assignment.
+  //! \param func : The new function to represent.
+  template<typename Callable>
+  Function& operator=(Callable func);
+  
+  //! \brief Unset the function.
+  void unset(void);
+  
+  //! \brief Tell wether the function is set (not NULL) or not.
+  //! \return True if the function is set, false otherwise.
+  inline bool isSet(void) const;
+  
+  //! \brief Functor.
+  //! \param ... : The function's parameters.
+  //! \return If non-void function, this returns the value returned by the function.
+  inline void operator()(...);
+  
+  //! \brief Tell wether the function is set (not NULL) or not.
+  //! \return True if the function is set, false otherwise.
+  inline operator bool(void) const;
+};
 
-//! \brief Binding for lambdas and normal functions
+//! \cond PRIVATE
 template<typename LambdaType, typename Ret, typename... Args>
 struct StaticBinder {
   static Ret exec(void* lambda, Args... args) {
@@ -26,100 +70,88 @@ template<typename Ret, typename... Args>
 class Function<Ret(Args...)> {
 private:
   typedef Ret(*FuncType)(void*, Args...);
-
-  //! \todo : Move
+  
   static inline void internal_memcpy(u8* p1, u8* p2, u16 size) {
     for(int i = 0 ;i < size ; i++) {
       *(p1++) = *(p2++);
     }
   }
-
-private:
-  //! \todo : Make it clean
+  
   u16 _size;
   u8* _data;
   FuncType _func;
-
+  
 public:
-  //! \brief Function assignment
+  inline Function(void)
+    : _size(0), _data(0), _func(0) {
+  }
+  
+  inline Function(const Function& other)
+    : Function() {
+    *this = other;
+  }
+  
+  template<typename Callable>
+  inline Function(Callable func)
+    : Function() {
+    *this = func;
+  }
+  
+  inline ~Function(void) {
+    if(_data) {
+      delete[] _data;
+    }
+  }
+  
+  Function& operator=(const Function& other) {
+    if(_data) {
+      delete[] _data;
+    }
+    
+    _size = other._size;
+    _data = new u8[_size];
+    internal_memcpy((u8*) _data, (u8*) other._data, _size);
+    _func = other._func;
+    
+    return *this;
+  }
+  
   template<typename Callable>
   Function& operator=(Callable func) {
     if(_data) {
-      delete _data;
+      delete[] _data;
     }
-
+    
     _size = sizeof(Callable);
-    _data = (u8*)malloc(_size);
-    internal_memcpy((u8*)_data, (u8*)&func, _size);
+    _data = new u8[_size];
+    internal_memcpy((u8*) _data, (u8*) &func, _size);
     _func = StaticBinder<Callable, Ret, Args...>::exec;
     
     return *this;
   }
-
-  //! \brief Copy operator
-  Function& operator=(Function& other) {
+  
+  void unset(void) {
     if(_data) {
-      delete _data;
+      delete[] _data;
     }
-
-    _size = other._size;
-    _data = new u8[_size];
-    internal_memcpy((u8*)_data, (u8*)other._data, _size);
-    _func = other._func;
-
-    return *this;
-  }
-
-  //! \brief Reset operator
-  /*
     
-    The function is null event if the parameter is not null.
-    
-   */
-  Function& operator=(int val) {
-    (void)val;
-    if(_data) {
-      delete _data;
-    }
-      
     _size = 0;
     _data = 0;
     _func = 0;
-
-    return *this;
-  }
-
-  //! \brief Default Constructor
-  Function(void)
-    : _size(0), _data(0), _func(0) {
-    
-  }
-
-  Function(int val) {
-    *this = val;
-  }
-
-  //! \brief Function constructor
-  template<typename Callable>
-  Function(Callable func)
-    : Function() {
-    *this = func;
-  }
-
-  //! \brief Destructor
-  ~Function(void) {
-    delete _data;
   }
   
-  //! \brief Function operator
+  inline bool isSet(void) const {
+    return _func != 0;    
+  }
+  
   inline Ret operator()(Args... args) const {
     return (*_func)(_data, args...);
   }
-
-  //! \brief If the function pointer is NULL or not.
+  
   inline operator bool(void) const {
-    return _func != 0;
+    return isSet();
   }
 };
+//! \endcond
 
 #endif//FUNCTION_HPP
