2d9432bf62040e3254c65ff66d0a03f51106e18c
astralien3000 <astralien3000@yahoo.fr>
2014-06-02 18:43:10 +0200

    Added license header to archi_parser v2

--- tools/archi_parser_v2/sub_register_nod.cpp
+++
@@ -1,8 +0,0 @@
-#include "sub_register_nod.hpp"
-
-SubRegisterNode::SubRegisterNode(Identifier id) : StructNode(id) {
-}
-
-int SubRegisterNode::getRegSize(void) {
-  return ((SubRegisterNode*)parent())->getRegSize();
-}
