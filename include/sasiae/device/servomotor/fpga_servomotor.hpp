64e0cf61b4b69d87d679d22263d2ee3d5e669825
astralien3000 <astralien3000@yahoo.fr>
2014-06-02 18:15:49 +0200

    Minor SASIAE Debug

--- include/sasiae/device/servomotor/fpga_servomotor.hpp
+++ include/sasiae/device/servomotor/fpga_servomotor.hpp
@@ -3,12 +3,12 @@
 
 #include "../../../common/device/servomotor/fpga_servomotor.hpp"
 
-template<typename T, T* ADDR>
+template<typename T, int ADDR>
 FpgaServomotor<T, ADDR>::FpgaServomotor(const char* name) : Servomotor(name) {}
 
-template<typename T, T* ADDR>
+template<typename T, int ADDR>
 void FpgaServomotor<T, ADDR>::setValue(u32 val) {
-  Servomotor::setValue(*ADDR = val);
+  Servomotor::setValue(*(T*)ADDR = val);
 }
 
 #endif//SASIAE_SERVOMOTOR_FPGA_HPP
