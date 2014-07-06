2e68f1a3573eca74cceda0118983f91018ab3bf8
Elioty <roadkiller.cl@gmail.com>
2014-07-07 00:05:56 +0100

    Adding some missing const attribute on certain Array's methods. Implementing Array specialisation for booleans.

--- include/common/base/array.hpp
+++ include/common/base/array.hpp
@@ -20,6 +20,7 @@
 #define ARRAY_HPP
 
 #include <base/integer.hpp>
+#include <base/bool_ref.hpp>
 
 typedef uf16 array_t;
 
@@ -27,6 +28,7 @@ typedef uf16 array_t;
 //! \brief Class representing a native array with operator overloading.
 //! \param _SIZE : the amount of elements in the array.
 //! \param _ElementType : the type of the elements contained within the array.
+//! \note This class is specialized for boolean arrays. See Array<_SIZE, bool> for more information.
 template<array_t _SIZE = 0, typename _ElementType = u8>
 class Array {
 public:
@@ -181,7 +183,7 @@ public:
   //! \brief Add each element of two arraies together two-by-two.
   //! \param other : the array to get the values to add from.
   //! \return The result array after the additions.
-  inline Array operator+(const Array& other) {
+  inline Array operator+(const Array& other) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] + other[i];
@@ -192,7 +194,7 @@ public:
   //! \brief Substract each element of two arraies together two-by-two.
   //! \param other : the array to get the values to substract from.
   //! \return The result array after the substractions.
-  inline Array operator-(const Array& other) {
+  inline Array operator-(const Array& other) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] - other[i];
@@ -203,7 +205,7 @@ public:
   //! \brief Multiply each element of two arraies together two-by-two.
   //! \param other : the array to get the multipliers from.
   //! \return The result array after the multiplications.
-  inline Array operator*(const Array& other) {
+  inline Array operator*(const Array& other) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] * other[i];
@@ -214,7 +216,7 @@ public:
   //! \brief Divide each element of two arraies together two-by-two.
   //! \param other : the array to get the divisors from.
   //! \return The result array after the divisions.
-  inline Array operator/(const Array& other) {
+  inline Array operator/(const Array& other) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] / other[i];
@@ -225,7 +227,7 @@ public:
   //! \brief Apply a modulo on each element of two arraies together two-by-two.
   //! \param other : the array to get the divisors from.
   //! \return The result array after the modulo operations.
-  inline Array operator%(const Array& other) {
+  inline Array operator%(const Array& other) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] % other[i];
@@ -286,7 +288,7 @@ public:
   //! \brief Add an oject to each element of the array.
   //! \param o : the object to add.
   //! \return The result array after the additions.
-  inline Array operator+(ElementConstRef o) {
+  inline Array operator+(ElementConstRef o) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] + o;
@@ -297,7 +299,7 @@ public:
   //! \brief Substract an object to each element of the array.
   //! \param o : the object to substract.
   //! \return The result array after the substractions.
-  inline Array operator-(ElementConstRef o) {
+  inline Array operator-(ElementConstRef o) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] - o;
@@ -308,7 +310,7 @@ public:
   //! \brief Multiply each element of the array by another object.
   //! \param o : the multiplier.
   //! \return The result array after the multiplications.
-  inline Array operator*(ElementConstRef o) {
+  inline Array operator*(ElementConstRef o) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] * o;
@@ -319,7 +321,7 @@ public:
   //! \brief Divide each element of the array by another object.
   //! \param o : the divisor.
   //! \return The result array after the divisions.
-  inline Array operator/(ElementConstRef o) {
+  inline Array operator/(ElementConstRef o) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] / o;
@@ -330,7 +332,328 @@ public:
   //! \brief Apply a modulo on each element of the array by another object.
   //! \param o : the divisor.
   //! \return The result array after the modulo operations.
-  inline Array operator%(ElementConstRef o) {
+  inline Array operator%(ElementConstRef o) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] % o;
+    }
+    return ret;
+  }
+};
+
+//! \class Array<_SIZE, bool> array.hpp <base/array.hpp>
+//! \brief Class representing a native array with operator overloading specialised for booleans.
+//! \param _SIZE : the amount of booleans in the array.
+template<array_t _SIZE>
+class Array<_SIZE, bool> {
+public:
+  //! \brief The size of the array.
+  static const array_t SIZE = _SIZE;
+  
+  //! \brief The length in bits for the array's field indexes.
+  static const array_t ARRAY_INDEX_LENGTH = Array<>::ARRAY_INDEX_LENGTH;
+  
+  //! \brief The maximum size of an array.
+  static const array_t MAX_SIZE = Array<>::MAX_SIZE;
+  
+  //! \brief Element's type.
+  typedef bool ElementType;
+  
+  //! \brief Element's reference type.
+  typedef BoolRef ElementRef;
+  
+  //! \brief Element's constant reference type.
+  typedef bool ElementConstRef;
+  
+private:
+  //! \brief The real size of the native C-like array.
+  static const array_t REAL_SIZE = (SIZE / 8) + ((SIZE % 8) ? 1 : 0);
+  
+  //! \brief The native C-like array containing the datas.
+  ElementType _elements[REAL_SIZE];
+  
+  //! \brief Variadic templated method to insert multiple elements at the construction of the array.
+  //! \param i : the index where to insert the next element in the array.
+  //! \param e : the next element to add to the array.
+  //! \param args : the remaining arguments.
+  template<typename... Targs>
+  inline void set(array_t i, ElementConstRef e, const Targs&... args) {
+    if(i < SIZE) {
+      (*this)[i] = e;
+      set(i+1, args...);
+    }
+  }
+  
+  //! \brief Termination method to insert multiple elements at the construction of the array.
+  //! \param i : The index where the list of value given ends.
+  //! \note It copies the last element at all the remaining indexes.
+  inline void set(array_t i) {
+    for(array_t j = i; j < SIZE; j++) {
+      (*this)[j] = (*this)[j-1];
+    }
+  }
+  
+public:
+  //! \brief Default constructor.
+  //! \attention The elements within the array are not set to any value.
+  inline Array(void) {
+  }
+  
+  //! \brief Variadic constructor to insert multiple elements.
+  //! \param args : the value list to insert in the array.
+  //! \note If there are less elements given than the size of the array, the last element is written at all the remaining indexes.
+  template<typename... Targs>
+  inline Array(const Targs&... args)
+    : Array() {
+    set(0, args...);
+  }
+  
+  //! \brief Copy constructor.
+  //! \param other : the array to copy.
+  inline Array(const Array& other) {
+    (*this) = other;
+  }
+  
+  //! \brief Copy operator.
+  //! \param other : the array to copy.
+  //! \return A reference to the array that has been written.
+  inline Array& operator=(const Array& other) {
+    for(array_t i = 0; i < REAL_SIZE; i++) {
+      // It is faster this way, booleans are copied 8 by 8.
+      _elements[i] = other._elements[i];
+    }
+    return (*this);
+  }
+  
+  //! \brief Assign a value to every element in the array.
+  //! \param e : the value to assign.
+  //! \return A reference to the array that has been written.
+  inline Array& operator=(ElementConstRef e) {
+    // It is faster this way, booleans are written 8 by 8.
+    u8 v = ((e) ? ~0 : 0);
+    for(array_t i = 0; i < REAL_SIZE; i++) {
+      _elements[i] = v;
+    }
+    return (*this);
+  }
+  
+  //! \brief Access to an element's reference via its field's index.
+  //! \param i : the index of the element to access.
+  //! \return The reference to the element contained at the specified index.
+  inline ElementRef operator[](array_t i) {
+    return BoolRef(&_elements[i / 8], i % 8);
+  }
+  
+  //! \brief Access to an element's constant reference via its field's index.
+  //! \param i : the index of the element to access.
+  //! \return The constant reference to the element contained at the specified index.
+  inline ElementConstRef operator[](array_t i) const {
+    return (bool) (_elements[i / 8] & (1 << (i % 8)));
+  }
+  
+  //! \brief Add to each element of the array the corresponding element from another array.
+  //! \param other : the array to get the values to add from.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator+=(const Array& other) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] += other[i];
+    }
+    return (*this);
+  }
+  
+  //! \brief Substract to each element of the array the corresponding element from another array.
+  //! \param other : the array to get the values to substract from.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator-=(const Array& other) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] -= other[i];
+    }
+    return (*this);
+  }
+  
+  //! \brief Multiply each element of the array by the corresponding element from another array.
+  //! \param other : the array to get the values to multiply with.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator*=(const Array& other) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] *= other[i];
+    }
+    return (*this);
+  }
+  
+  //! \brief Divide each element of the array by the corresponding element from another array.
+  //! \param other : the array to get the divisors from.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator/=(const Array& other) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] /= other[i];
+    }
+    return (*this);
+  }
+  
+  //! \brief Get the modulo of each element of the array by the corresponding element from another array.
+  //! \param other : the array to get the divisors from.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator%=(const Array& other) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] %= other[i];
+    }
+    return (*this);
+  }
+  
+  //! \brief Add each element of two arraies together two-by-two.
+  //! \param other : the array to get the values to add from.
+  //! \return The result array after the additions.
+  inline Array operator+(const Array& other) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] + other[i];
+    }
+    return ret;
+  }
+  
+  //! \brief Substract each element of two arraies together two-by-two.
+  //! \param other : the array to get the values to substract from.
+  //! \return The result array after the substractions.
+  inline Array operator-(const Array& other) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] - other[i];
+    }
+    return ret;
+  }
+  
+  //! \brief Multiply each element of two arraies together two-by-two.
+  //! \param other : the array to get the multipliers from.
+  //! \return The result array after the multiplications.
+  inline Array operator*(const Array& other) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] * other[i];
+    }
+    return ret;
+  }
+  
+  //! \brief Divide each element of two arraies together two-by-two.
+  //! \param other : the array to get the divisors from.
+  //! \return The result array after the divisions.
+  inline Array operator/(const Array& other) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] / other[i];
+    }
+    return ret;
+  }
+  
+  //! \brief Apply a modulo on each element of two arraies together two-by-two.
+  //! \param other : the array to get the divisors from.
+  //! \return The result array after the modulo operations.
+  inline Array operator%(const Array& other) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] % other[i];
+    }
+    return ret;
+  }
+  
+  //! \brief Add to each element of the array another object.
+  //! \param o : the object to add.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator+=(ElementConstRef o) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] += o;
+    }
+    return (*this);
+  }
+  
+  //! \brief Substract to each element of the array another object.
+  //! \param o : the object to substract.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator-=(ElementConstRef o) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] -= o;
+    }
+    return (*this);
+  }
+  
+  //! \brief Multiply each element of the array with another object.
+  //! \param o : the multiplier.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator*=(ElementConstRef o) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] *= o;
+    }
+    return (*this);
+  }
+  
+  //! \brief Divide each element of the array by another object.
+  //! \param o : the divisor.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator/=(ElementConstRef o) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] /= o;
+    }
+    return (*this);
+  }
+  
+  //! \brief Get the modulo of each element of the array by another object.
+  //! \param o : the divisor.
+  //! \return A reference to the array the assignment has been done to.
+  inline Array& operator%=(ElementConstRef o) {
+    for(array_t i = 0; i < SIZE; i++) {
+      (*this)[i] %= o;
+    }
+    return (*this);
+  }
+  
+  //! \brief Add an oject to each element of the array.
+  //! \param o : the object to add.
+  //! \return The result array after the additions.
+  inline Array operator+(ElementConstRef o) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] + o;
+    }
+    return ret;
+  }
+  
+  //! \brief Substract an object to each element of the array.
+  //! \param o : the object to substract.
+  //! \return The result array after the substractions.
+  inline Array operator-(ElementConstRef o) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] - o;
+    }
+    return ret;
+  }
+  
+  //! \brief Multiply each element of the array by another object.
+  //! \param o : the multiplier.
+  //! \return The result array after the multiplications.
+  inline Array operator*(ElementConstRef o) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] * o;
+    }
+    return ret;
+  }
+  
+  //! \brief Divide each element of the array by another object.
+  //! \param o : the divisor.
+  //! \return The result array after the divisions.
+  inline Array operator/(ElementConstRef o) const {
+    Array ret;
+    for(array_t i = 0; i < SIZE; i++) {
+      ret[i] = (*this)[i] / o;
+    }
+    return ret;
+  }
+  
+  //! \brief Apply a modulo on each element of the array by another object.
+  //! \param o : the divisor.
+  //! \return The result array after the modulo operations.
+  inline Array operator%(ElementConstRef o) const {
     Array ret;
     for(array_t i = 0; i < SIZE; i++) {
       ret[i] = (*this)[i] % o;
