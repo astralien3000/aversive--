0671c82eff72fe9ea3af3cf0484e8833795f1a8f
Elioty <roadkiller.cl@gmail.com>
2014-07-30 21:56:30 +0100

    Updating BoolRef test to the new test system.

--- tests/common/base/bool_ref/main.cpp
+++ tests/common/base/bool_ref/main.cpp
@@ -1,5 +1,6 @@
 #include <base/bool_ref.hpp>
 #include <iostream>
+#include "../../../my_assert.hpp"
 
 int main(int argc, char** argv) {
   (void) argc;
@@ -8,92 +9,56 @@ int main(int argc, char** argv) {
   bool b1 = true;
   BoolRef r1(b1);
   
-  if(!r1) {
-    std::cout << "NOK" << std::endl;
-    return 1;
-  }
+  myAssert(r1, "Line " S__LINE__ ": Construction to reference a bool, r1 should be true.");
   
   r1 = false;
   
-  if(r1 || b1) {
-    std::cout << "NOK" << std::endl;
-    return 1;
-  }
+  myAssert(!r1 && !b1, "Line " S__LINE__ ": Reference problem, r1 and b1 should be false.");
   
   BoolRef r2(r1);
   
-  if(r2 || r1 || b1) {
-    std::cout << "NOK" << std::endl;
-    return 1;
-  }
+  myAssert(!r1 && !r2 && !b1, "Line " S__LINE__ ": Copy construction, r1, r2 and b1 should be false.");
   
   r2 = true;
   
-  if(!(r2 && r1 && b1)) {
-    std::cout << "NOK" << std::endl;
-    return 1;
-  }
+  myAssert(r1 && r2 && b1, "Line " S__LINE__ ": Reference problem, r1, r2 and b1 should be true.");
   
   u8 b2 = 0xF0;
   
   for(int i = 0; i < 4; i++) {
     BoolRef r(&b2, i);
-    if(r) {
-      std::cout << "NOK" << std::endl;
-      return 1;
-    }
+    myAssert(!r, "Line " S__LINE__ ": Construction to reference a bit within a byte, r should be false.");
     r = !r;
   }
   
   for(int i = 4; i < 8; i++) {
     BoolRef r(&b2, i);
-    if(!r) {
-      std::cout << "NOK" << std::endl;
-      return 1;
-    }
+    myAssert(r, "Line " S__LINE__ ": Construction to reference a bit within a byte, r should be true.");
     r = !r;
   }
   
-  if(b2 != 0x0F) {
-    std::cout << "NOK" << std::endl;
-    return 1;
-  }
-
+  myAssert(b2 == 0x0F, "Line " S__LINE__ ": Reference to a bit within a byte problem, b2 must be equal to 0x0F.");
+  
   r1 = true;
   r1 += true;
-  if(r1 != (bool) (true + true)) {
-    std::cout << "NOK +=" << std::endl;
-    return 1;
-  }
-
+  myAssert(r1 == (bool) (true + true), "Line " S__LINE__ ": += operator problem.");
+  
   r1 = true;
   r1 -= true;
-  if(r1 != (bool) (true - true)) {
-    std::cout << "NOK -=" << std::endl;
-    return 1;
-  }
-
+  myAssert(r1 == (bool) (true - true), "Line " S__LINE__ ": -= operator problem.");
+  
   r1 = true;
   r1 *= false;
-  if(r1 != (bool) (true * false )) {
-    std::cout << "NOK *=" << std::endl;
-    return 1;
-  }
-
+  myAssert(r1 == (bool) (true * false), "Line " S__LINE__ ": *= operator problem.");
+  
   r1 = true;
   r1 /= true;
-  if(r1 != (bool) (true / true)) {
-    std::cout << "NOK /=" << std::endl;
-    return 1;
-  }
-
+  myAssert(r1 == (bool) (true / true), "Line " S__LINE__ ": /= operator problem.");
+  
   r1 = true;
   r1 %= true;
-  if(r1 != (bool) (true % true)) {
-    std::cout << "NOK %=" << std::endl;
-    return 1;
-  }
-
+  myAssert(r1 == (bool) (true % true), "Line " S__LINE__ ": %= operator problem.");
+  
   std::cout << "OK" << std::endl;  
   return 0;
 }
