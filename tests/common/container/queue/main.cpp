158cd6ea321f5f00eb6baaa72dde526dbc859f75
Elioty <roadkiller.cl@gmail.com>
2014-08-03 18:00:01 +0100

    Updating Queue test.

--- tests/common/container/queue/main.cpp
+++ tests/common/container/queue/main.cpp
@@ -1,6 +1,6 @@
 #include <container/queue.hpp>
 #include <iostream>
-#include <cassert>
+#include "../../../my_assert.hpp"
 
 static const queue_t SIZE = 10;
 
@@ -8,32 +8,32 @@ int main(int argc, char** argv) {
   (void) argc;
   (void) argv;
   
-  Queue<SIZE, u8> q(10,5,8,20);
+  Queue<SIZE, u8> q(10, 5, 8, 20);
   
-  assert(q.head() == 10);
-  assert(q.usedSpace() == 4);
-  assert(q.freeSpace() == (SIZE) - q.usedSpace());
-  assert(q.enqueue(12));
-  assert(q.usedSpace() == 5);
-  assert(q.enqueue(2));
-  assert(q.head() == 10);
-  assert(q.dequeue());
-  assert(q.head() == 5);
-  assert(!q.isFull());
-  assert(!q.isEmpty());
+  myAssert(q.head() == 10, "Line " S__LINE__": Queue<SIZE, u8>::head(void).");
+  myAssert(q.usedSpace() == 4, "Line " S__LINE__": Queue<SIZE, u8>::usedSpace(void).");
+  myAssert(q.freeSpace() == (SIZE - q.usedSpace()), "Line " S__LINE__": Queue<SIZE, u8>::freeSpace(void).");
+  myAssert(q.enqueue(12), "Line " S__LINE__": Queue<SIZE, u8>::enqueue(u8).");
+  myAssert(q.usedSpace() == 5, "Line " S__LINE__": Queue<SIZE, u8>::usedSpace(void).");
+  myAssert(q.enqueue(2), "Line " S__LINE__": Queue<SIZE, u8>::enqueue(u8).");
+  myAssert(q.head() == 10, "Line " S__LINE__": Queue<SIZE, u8>::head(void).");
+  myAssert(q.dequeue(), "Line " S__LINE__": Queue<SIZE, u8>::dequeue(void).");
+  myAssert(q.head() == 5, "Line " S__LINE__": Queue<SIZE, u8>::head(void).");
+  myAssert(!q.isFull(), "Line " S__LINE__": Queue<SIZE, u8>::isFull(void).");
+  myAssert(!q.isEmpty(), "Line " S__LINE__": Queue<SIZE, u8>::isEmpty(void).");
   
   q.flush();
   
-  assert(q.usedSpace() == 0);
+  myAssert(q.usedSpace() == 0, "Line " S__LINE__": Queue<SIZE, u8>::flush(void).");
   for(queue_t i = 0; i < 5; i++) {
-    assert(q.enqueue(10+i));
+    myAssert(q.enqueue(10 + i), "Line " S__LINE__": Queue<SIZE, u8>::enqueue(u8).");
   }
   
-  assert(q.usedSpace() == 5);
+  myAssert(q.usedSpace() == 5, "Line " S__LINE__": Queue<SIZE, u8>::usedSpace(void).");
   for(queue_t i = 0; i < 5; i++) {
-    assert(q.head() == (10+i));
-    assert(q.dequeue());
-    assert(q.usedSpace() == (4-i));
+    myAssert(q.head() == (10 + i), "Line " S__LINE__": Queue<SIZE, u8>::head(void).");
+    myAssert(q.dequeue(), "Line " S__LINE__": Queue<SIZE, u8>::dequeue(void).");
+    myAssert(q.usedSpace() == (4 - i), "Line " S__LINE__": Queue<SIZE, u8>::usedSpace(void).");
   }
   
   std::cout << "OK" << std::endl;
