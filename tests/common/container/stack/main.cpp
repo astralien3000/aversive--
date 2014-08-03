d1c28f558ba9d558012d9577c87ad9249628529b
Elioty <roadkiller.cl@gmail.com>
2014-08-03 18:06:35 +0100

    Updating Stack test.

--- tests/common/container/stack/main.cpp
+++ tests/common/container/stack/main.cpp
@@ -1,6 +1,6 @@
 #include <container/stack.hpp>
 #include <iostream>
-#include <cassert>
+#include "../../../my_assert.hpp"
 
 static const stack_t SIZE = 10;
 
@@ -8,32 +8,32 @@ int main(int argc, char** argv) {
   (void) argc;
   (void) argv;
   
-  Stack<SIZE, u8> s(10,5,8,20);
+  Stack<SIZE, u8> s(10, 5, 8, 20);
   
-  assert(s.head() == 20);
-  assert(s.usedSpace() == 4);
-  assert(s.freeSpace() == (SIZE) - s.usedSpace());
-  assert(s.push(12));
-  assert(s.usedSpace() == 5);
-  assert(s.push(2));
-  assert(s.head() == 2);
-  assert(s.pop());
-  assert(s.head() == 12);
-  assert(!s.isFull());
-  assert(!s.isEmpty());
+  myAssert(s.head() == 20, "Line " S__LINE__": Stack<SIZE, u8>::head(void).");
+  myAssert(s.usedSpace() == 4, "Line " S__LINE__": Stack<SIZE, u8>::usedSpace(void).");
+  myAssert(s.freeSpace() == (SIZE - s.usedSpace()), "Line " S__LINE__": Stack<SIZE, u8>::freeSpace(void).");
+  myAssert(s.push(12), "Line " S__LINE__": Stack<SIZE, u8>::push(u8).");
+  myAssert(s.usedSpace() == 5, "Line " S__LINE__": Stack<SIZE, u8>::usedSpace(void).");
+  myAssert(s.push(2), "Line " S__LINE__": Stack<SIZE, u8>::push(u8).");
+  myAssert(s.head() == 2, "Line " S__LINE__": Stack<SIZE, u8>::head(void).");
+  myAssert(s.pop(), "Line " S__LINE__": Stack<SIZE, u8>::pop(void).");
+  myAssert(s.head() == 12, "Line " S__LINE__": Stack<SIZE, u8>::head(void).");
+  myAssert(!s.isFull(), "Line " S__LINE__": Stack<SIZE, u8>::isFull(void).");
+  myAssert(!s.isEmpty(), "Line " S__LINE__": Stack<SIZE, u8>::isEmpty(void).");
   
   s.flush();
   
-  assert(s.usedSpace() == 0);
+  myAssert(s.usedSpace() == 0, "Line " S__LINE__": Stack<SIZE, u8>::flush(void).");
   for(stack_t i = 0; i < 5; i++) {
-    assert(s.push(10+i));
+    myAssert(s.push(10 + i), "Line " S__LINE__": Stack<SIZE, u8>::push(u8).");
   }
   
-  assert(s.usedSpace() == 5);
+  myAssert(s.usedSpace() == 5, "Line " S__LINE__": Stack<SIZE, u8>::usedSpace(void).");
   for(int i = 4; i >= 0; i--) {
-    assert(s.head() == (10+i));
-    assert(s.pop());
-    assert(s.usedSpace() == (stack_t) i);
+    myAssert(s.head() == (10 + i), "Line " S__LINE__": Stack<SIZE, u8>::head(void).");
+    myAssert(s.pop(), "Line " S__LINE__": Stack<SIZE, u8>::pop(void).");
+    myAssert(s.usedSpace() == (stack_t) i, "Line " S__LINE__": Stack<SIZE, u8>::usedSpace(void).");
   }
   
   std::cout << "OK" << std::endl;
