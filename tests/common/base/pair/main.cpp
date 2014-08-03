836a2a2a6aa8c288a9aa1416f5172e2f099f5410
Elioty <roadkiller.cl@gmail.com>
2014-08-03 15:10:01 +0100

    Completing Pair test.

--- tests/common/base/pair/main.cpp
+++ tests/common/base/pair/main.cpp
@@ -6,6 +6,15 @@ int main(int argc, char** argv) {
   (void) argc;
   (void) argv;
   
+  Pair<u8, u8> p5(42, 12);  
+  myAssert(p5.left() == 42 && p5.right() == 12, "Line " S__LINE__ ": Pair<u8, u8> construction.");
+  
+  p5.left() = 53;
+  myAssert(p5.left() == 53 && p5.right() == 12, "Line " S__LINE__ ": Pair<u8, u8> left reference.");
+  
+  p5.right() = 13;
+  myAssert(p5.left() == 53 && p5.right() == 13, "Line " S__LINE__ ": Pair<u8, u8> right reference.");
+  
   myAssert(!(sizeof(Pair<bool, bool>) >= sizeof(Pair<u8, u8>)),
            "Line " S__LINE__ ": A boolean Pair is not lighter than a byte Pair even though it should have a bool specialisation.");
   
