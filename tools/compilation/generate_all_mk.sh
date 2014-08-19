d0f350fc7ee4e8e4d5a1a265dbaa5f6b3f9d6715
astralien3000 <astralien3000@yahoo.fr>
2014-08-19 10:15:41 +0200

    Debug compilation script

--- tools/compilation/generate_all_mk.sh
+++ tools/compilation/generate_all_mk.sh
@@ -8,7 +8,7 @@ GENERATE_RULE=tools/compilation/generate_rule.sh
 ########################################
 # SASIAE
 
-$GENERATE_MK sasiae
+$GENERATE_MK sasiae &> /dev/null
 $GENERATE_RULE sasiae
 TARGET_LIST+="sasiae "
 
@@ -21,7 +21,7 @@ for i in $AVR_MMCUS
 do
   if [ ${i%.*} == ${i%%.*} ]
   then
-    $GENERATE_MK avr ${i%.*}
+    $GENERATE_MK avr ${i%.*} &> /dev/null
     $GENERATE_RULE ${i%.*}
     TARGET_LIST+="${i%.*} "
   fi
@@ -30,7 +30,7 @@ done
 ########################################
 # STM32
 
-$GENERATE_MK stm32 stm32f4
+$GENERATE_MK stm32 stm32f4 &> /dev/null
 $GENERATE_RULE stm32f4
 TARGET_LIST+="stm32f4 "
 
