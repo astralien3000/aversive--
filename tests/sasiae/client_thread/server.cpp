f75af9c2a1e7a5f85bd930b99adacd8e7e9983f7
Elioty <roadkiller.cl@gmail.com>
2014-08-22 16:37:04 +0100

    Factorizing some SASIAE-specific test helper functions. Updating root .gitignore file.

--- tests/sasiae/client_thread/server.cpp
+++ tests/sasiae/client_thread/server.cpp
@@ -1,44 +1,7 @@
 #include <QProcess>
-#include <cstring>
 #include <iostream>
 #include "../../my_assert.hpp"
-
-static constexpr int BUFFER_SIZE = 1024;
-
-char buffer[BUFFER_SIZE];
-
-bool readLine(QProcess& client) {
-  unsigned int i = 0;
-  char c;
-  if(!client.getChar(&c)) {
-    client.waitForReadyRead();
-  }
-  else {
-    buffer[i++] = c;
-  }
-  
-  while(client.getChar(&c) && c != '\n') {
-    buffer[i++] = c;
-  }
-  buffer[i] = '\0';
-  
-  if(c == '\n') {
-    return true;
-  }
-  else {
-    client.kill();
-    client.waitForFinished();
-    return false;
-  }
-}
-
-bool checkMsg(QProcess& client, const char* str) {
-  if(!readLine(client)) {
-    return false;
-  }
-  
-  return !strcmp(buffer, str);
-}
+#include "../util.hpp"
 
 int main(int argc, char** argv) {
   (void) argc;
