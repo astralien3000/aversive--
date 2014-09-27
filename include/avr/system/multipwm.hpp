37d98fd70e5e5d875def86c70b29e51bff45432d
Elioty <roadkiller.cl@gmail.com>
2014-09-27 14:55:12 +0100

    Adding missing license headers in MultiPWM header files. Adding a static_assert in the MultiPWM header files for the yet unsupported platforms. Some other minor things regarding coding conventions.

--- include/avr/system/multipwm.hpp
+++ include/avr/system/multipwm.hpp
@@ -1,3 +1,21 @@
+/*
+    Aversive++                                                           
+    Copyright (C) 2014 Eirbot                                            
+                                                                         
+    This program is free software: you can redistribute it and/or modify 
+    it under the terms of the GNU General Public License as published by 
+    the Free Software Foundation, either version 3 of the License, or    
+    (at your option) any later version.                                  
+                                                                         
+    This program is distributed in the hope that it will be useful,      
+    but WITHOUT ANY WARRANTY; without even the implied warranty of       
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+    GNU General Public License for more details.                         
+                                                                         
+    You should have received a copy of the GNU General Public License    
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef AVR_MULTIPWM_HPP
 #define AVR_MULTIPWM_HPP
 
@@ -7,29 +25,29 @@
 
 struct DefaultMultiPWMConfig {
   static constexpr u32 MAX_PINS = 8;
-
+  
   static constexpr u32 TIMER_ID = 0;
   static constexpr u32 TIMER_COUNTER_SIZE = 8;
   static constexpr u32 TIMER_PRESCALER = 1;
-
+  
   static constexpr double TIMER_FREQUENCY = 16000000;
-
+  
   static constexpr u32 PERIOD = 20000;
 };
 
-template<class Config>
+template<typename Config>
 MultiPWM<Config>::MultiPWM(void) {
   static constexpr u32 OVERFLOW = (1 << Config::TIMER_COUNTER_SIZE);
   static constexpr u32 OVERFLOW_FREQUENCY = Config::TIMER_FREQUENCY / (Config::TIMER_PRESCALER * OVERFLOW);
   static constexpr u32 OVERFLOW_PERIOD = 1000000 / OVERFLOW_FREQUENCY;
-
+  
   _current = 0;
   Timer<Config::TIMER_ID>::init();
   Timer<Config::TIMER_ID>::template setPrescaler<Config::TIMER_PRESCALER>();
   Timer<Config::TIMER_ID>::overflowEvent().setFunction([]() {
-    MultiPWM::instance()._current += OVERFLOW_PERIOD;
-    MultiPWM::instance().process();
-  });
+      MultiPWM::instance()._current += OVERFLOW_PERIOD;
+      MultiPWM::instance().process();
+    });
   Timer<Config::TIMER_ID>::overflowEvent().start();
 }
 
--- tests/multipwm/main.cpp
+++ tests/multipwm/main.cpp
@@ -4,9 +4,8 @@
 
 #include <hardware/interrupts.hpp>
 
-
-constexpr u32 MAX = 2100;
-constexpr u32 MIN = 550;
+static constexpr u32 MAX = 2100;
+static constexpr u32 MIN = 550;
 
 template<u32 PIN_ID>
 class ServoTester {
@@ -15,24 +14,24 @@ private:
   MultiPWM<DefaultMultiPWMConfig>::Pin _servo;
   u32 _counter;
   u32 _dir;
-
+  
 public:
   ServoTester(void)
     : _pin(""),
       _servo(MultiPWM<DefaultMultiPWMConfig>::instance().createPin(_pin)) {
-
+    
     _pin.setMode(PinMode::OUTPUT);
-
+    
     _counter = MIN;
     _dir = 1;
-
+    
     _servo.setValue(_counter);
   }
-
+  
   void update(void) {
     _counter += _dir;
     if(_counter <= MIN || MAX <= _counter) _dir = -_dir;
-
+    
     _servo.setValue(_counter);
   }
 };
@@ -42,18 +41,19 @@ int main(int argc, char** argv) {
   (void) argv;
   
   Aversive::init();
-
+  
   ServoTester<12> test1; // D10
   ServoTester<13> test2; // D11
   ServoTester<14> test3; // D12
-
+  
   Interrupts::set();
-
+  
   while(Aversive::sync()) {
     test1.update();
     test2.update();
     test3.update();
-    for(volatile u32 t = 0 ; t < 2 ; t++);
+    for(volatile u32 t = 0 ; t < 2 ; t++) {
+    }
   }
   
   Aversive::setReturnCode(0);
