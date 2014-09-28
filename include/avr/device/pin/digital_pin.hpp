9dc8fa46050f85397514dac3c543834cb3cca52f
astralien3000 <astralien3000@yahoo.fr>
2014-09-28 22:37:56 +0200

    Implementation of DigitalPin for AVR

--- include/avr/device/pin/digital_pin.hpp
+++ include/avr/device/pin/digital_pin.hpp
@@ -3,6 +3,44 @@
 
 #include "../../../common/device/pin/digital_pin.hpp"
 
+#include <hardware/pinmap.hpp>
+
+template<int ID>
+inline DigitalPin<ID>::DigitalPin(const char *name)
+  : InputDigitalPin<ID>(name) {
+}
+
+template<int ID>
+inline void DigitalPin<ID>::setValue(bool value) {
+  if(isOutput()) {
+    OutputDigitalPin<ID>::setValue(value);
+  }
+}
+
+template<int ID>
+inline bool DigitalPin<ID>::getValue(void) {
+  return OutputDigitalPin<ID>::getValue();
+}
+
+template<int ID>
+inline void DigitalPin<ID>::setInput(void) {
+  Pinmap::Pin<ID>::setMode(Pinmap::IN);
+}
+
+template<int ID>
+inline void DigitalPin<ID>::setOutput(void) {
+  Pinmap::Pin<ID>::setMode(Pinmap::OUT);
+}
+
+template<int ID>
+inline bool DigitalPin<ID>::isInput(void) {
+  return Pinmap::Pin<ID>::mode() == Pinmap::IN;
+}
+
+template<int ID>
+inline bool DigitalPin<ID>::isOutput(void) {
+  return Pinmap::Pin<ID>::mode() == Pinmap::OUT;
+}
 
 
 #endif//AVR_PIN_HPP
