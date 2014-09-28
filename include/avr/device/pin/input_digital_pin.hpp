f58e5376806cc7311d24d7ecb9d414b1c51624d4
astralien3000 <astralien3000@yahoo.fr>
2014-09-28 20:55:53 +0200

    AVR implementation of InputDigitalPin

--- include/avr/device/pin/input_digital_pin.hpp
+++ include/avr/device/pin/input_digital_pin.hpp
@@ -3,6 +3,23 @@
 
 #include "../../../common/device/pin/input_digital_pin.hpp"
 
+#include <hardware/pinmap.hpp>
 
+template<int ID>
+inline InputDigitalPin<ID>::InputDigitalPin(const char *name, bool init)
+  : Device(name) {
+  (void) init;
+}
+
+template<int ID>
+inline InputDigitalPin<ID>::InputDigitalPin(const char *name)
+  : InputDigitalPin(name, false) {
+  Pinmap::Pin<ID>::setMode(Pinmap::IN);
+}
+
+template<int ID>
+inline bool InputDigitalPin<ID>::getValue(void) {
+  return Pinmap::Pin<ID>::read();
+}
 
 #endif//AVR_INPUT_PIN_HPP
