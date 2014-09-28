82ab75ae741f3abd90fad18f43c45ec59ca74cdf
astralien3000 <astralien3000@yahoo.fr>
2014-09-28 23:14:51 +0200

    Implemented InputDigitalPin for STM32

--- include/stm32/device/pin/input_digital_pin.hpp
+++ include/stm32/device/pin/input_digital_pin.hpp
@@ -3,6 +3,26 @@
 
 #include "../../../common/device/pin/input_digital_pin.hpp"
 
+#include <hardware/pinmap.hpp>
+
+template<int ID>
+inline InputDigitalPin<ID>::InputDigitalPin(const char *name, bool init)
+  : Device(name) {
+  (void) init;
+  Pinmap::Pin<ID>::initGpio();
+}
+
+template<int ID>
+inline InputDigitalPin<ID>::InputDigitalPin(const char *name)
+  : InputDigitalPin(name, false) {
+  Pinmap::Pin<ID>::setMode(Pinmap::IN);
+}
+
+template<int ID>
+inline bool InputDigitalPin<ID>::getValue(void) {
+  // TODO
+  return false;
+}
 
 
 #endif//STM32_INPUT_DIGITAL_PIN_HPP
