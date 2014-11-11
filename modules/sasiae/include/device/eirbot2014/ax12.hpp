fbcd95fe4385c9f09eed6b659026a123da91331d
astralien3000 <astralien3000@yahoo.fr>
2014-11-11 02:14:25 +0100

    Moved sasiae in modules dir

--- modules/sasiae/include/device/eirbot2014/ax12.hpp
+++ modules/sasiae/include/device/eirbot2014/ax12.hpp
@@ -33,6 +33,48 @@ struct AX12PrivateData{
   ComMode cm;
 };
 
-#include "../../../common/device/eirbot2014/ax12.hpp"
+
+#include <device/input.hpp>
+#include <device/output.hpp>
+#include <device/device.hpp>
+
+#include <base/integer.hpp>
+
+//! \brief A digital servomotor, used by Eirbot in 2014
+/*!
+
+  This actuator is very specific because he works as an extern card so
+  you can communicate with him. It is based on UART communication
+  that's why you should read UartStream instructions.
+
+*/
+class AX12: public Device, Input<u16>, Output<u16> {
+private:
+  AX12PrivateData data;
+
+public:
+  enum WorkingMode{WHEEL,JOIN};
+  enum ComMode{POSITION,SPEED,TORQUE};
+
+public:
+  AX12(char*);
+  ~AX12();
+
+  u16 getValue();
+  void setValue(u16);
+
+  u16 getAngle();
+  u16 getSpeed();
+  u16 getTorque();
+
+  void setAngle(u16);
+  void setSpeed(u16);
+
+  void setRecvMode(ComMode);
+  void setSendMode(WorkingMode);
+
+  void setAngleMax(u16, u16);
+  void setTorqueMax(u16);
+};
 
 #endif//SASIAE_AX12_HPP
--- modules/sasiae/include/device/eirbot2014/gp2.hpp
+++ modules/sasiae/include/device/eirbot2014/gp2.hpp
@@ -16,9 +16,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef GP2_HPP
-#define GP2_HPP
-
+#ifndef SASIAE_GP2_HPP
+#define SASIAE_GP2_HPP
 
 #include <device/device.hpp>
 #include <device/input.hpp>
@@ -46,4 +45,45 @@ private:
   u16 _last_val;
 };
 
-#endif//GP2_HPP
+
+/////////////////////////////////////////////////////////////////////
+
+#include <aversive.hpp>
+#include <client_thread.hpp>
+#include <sstream>
+
+template<u8 ID>
+GP2<ID>::GP2(const char* name)
+  : Device(name),
+    _last_val(0) {
+  Aversive::init();
+
+  ClientThread::instance().
+    registerDevice(*this,
+		   std::function<void(char*)>([&] (char* msg) mutable -> void {
+		       using namespace std;
+		       string cmd;
+		       long long val;
+		       istringstream iss(msg);
+		       
+		       iss >> cmd;
+		       
+		       if(cmd == "value") {
+			 iss >> val;
+			 _last_val = val;
+		       }
+		       else {
+			 ClientThread::instance().sendMessage(ClientThread::ERROR, "unable to parse message correctly");
+		       }
+		     }));
+
+  ClientThread::instance().
+    sendDeviceMessage(*this, "init");
+}
+
+template<u8 ID>
+u16 GP2<ID>::getValue(void) {
+  return _last_val;
+}
+
+#endif//SASIAE_GP2_HPP
