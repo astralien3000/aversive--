4a9838885ce5509ea90291b55a43e537780be9cd
Elioty <roadkiller.cl@gmail.com>
2015-07-01 22:36:04 +0100

    Adding thridparty source support to CMakeLists.txt. Refactoring many .cmake files. Adding missing compile flags to stm32f4.cmake. Adding asm source file support to the buildchain. Fixing generic.cmake -std compile flag for C.

--- cmake/toolchain/arm/stm32f4.cmake
+++ cmake/toolchain/arm/stm32f4.cmake
@@ -3,7 +3,21 @@ cmake_policy(SET CMP0011 NEW)
 
 set(AVERSIVE_PLATFORM "stm32")
 set(AVERSIVE_CPU "cortex-m4")
+set(AVERSIVE_THIRDPARTY core_cm4.h core_cm4_simd.h core_cmFunc.h core_cmInstr.h startup_stm32f40_41xxx.s stm32f4xx.h syscalls.c system_stm32f4xx.c system_stm32f4xx.h)
+
+set(res)
+foreach(l ${AVERSIVE_THIRDPARTY})
+  list(APPEND res "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/stm32f4xx/${l}")
+endforeach()
+set(AVERSIVE_THIRDPARTY ${res})
+unset(res)
+
 include("${CMAKE_CURRENT_LIST_DIR}/../arm.cmake")
 
-set(CMAKE_C_FLAGS      "${CMAKE_C_FLAGS_INIT} -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -Wdouble-promotion -ffunction-sections -fdata-sections -DSTM32F40_41xxx" CACHE STRING "")
-set(CMAKE_CXX_FLAGS    "${CMAKE_CXX_FLAGS_INIT} -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -Wdouble-promotion -ffunction-sections -fdata-sections -DSTM32F40_41xxx" CACHE STRING "")
+set(STM32F4_FLAGS "-mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -Wdouble-promotion -ffunction-sections -fdata-sections -DSTM32F40_41xxx -DDEBUG -DUSART_DEBUG=USART1 -DHSE_VALUE=168000000 -I${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/stm32f4xx")
+
+set(CMAKE_C_FLAGS      "${CMAKE_C_FLAGS_INIT} ${STM32F4_FLAGS}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS    "${CMAKE_CXX_FLAGS_INIT} ${STM32F4_FLAGS}" CACHE STRING "")
+set(CMAKE_ASM_FLAGS    "${CMAKE_ASM_FLAGS_INIT} ${STM32F4_FLAGS}" CACHE STRING "")
+
+unset(STM32F4_FLAGS)
--- cmake/toolchain/avr.cmake
+++ cmake/toolchain/avr.cmake
@@ -5,9 +5,14 @@ set(CMAKE_SYSTEM_NAME Generic)
 
 include("${CMAKE_CURRENT_LIST_DIR}/generic.cmake")
 
+set(AVR_FLAGS "-Os -mcall-prologues -I${CMAKE_CURRENT_SOURCE_DIR}/include/avr -mmcu=${AVERSIVE_MCU}")
+
 set(AVERSIVE_PLATFORM    avr)
 set(CMAKE_C_COMPILER     avr-gcc)
-set(CMAKE_C_FLAGS_INIT   "${CMAKE_C_FLAGS_INIT} -Os -mcall-prologues -I${CMAKE_CURRENT_SOURCE_DIR}/include/avr -mmcu=${AVERSIVE_MCU}")
+set(CMAKE_C_FLAGS_INIT   "${CMAKE_C_FLAGS_INIT} ${AVR_FLAGS}")
 set(CMAKE_CXX_COMPILER   avr-g++)
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -Os -mcall-prologues -I${CMAKE_CURRENT_SOURCE_DIR}/include/avr -mmcu=${AVERSIVE_MCU}")
-set(CMAKE_AR             avr-ar)
+set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} ${AVR_FLAGS} -fno-rtti -fno-exceptions")
+set(CMAKE_ASM_COMPILER   avr-gcc)
+set(CMAKE_ASM_FLAGS_INIT "${CMAKE_ASM_FLAGS_INIT} ${AVR_FLAGS}")
+
+unset(AVR_FLAGS)
