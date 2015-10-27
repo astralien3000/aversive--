9bd8664534a52b0eb8adb7bed23fabb40971343d
astralien3000 <astralien3000@yahoo.fr>
2015-10-27 18:12:15 +0100

    Fusion of register.hpp and register_impl.hpp

--- hdl/common/include/util/memory_mapping/register.hpp
+++ hdl/common/include/util/memory_mapping/register.hpp
@@ -20,21 +20,47 @@ namespace MemoryMapping {
 
   public:
     //! \brief Constructor in the case the address is a pointer
-    constexpr Register(RegType* const address);
+    constexpr Register(RegType* const address)
+      : ADDRESS((usys)address) {
+    }
     
     //! \brief Constructor in the case the address is a pointer to volatile
-    constexpr Register(volatile RegType* const address);
+    constexpr Register(volatile RegType* const address)
+      : ADDRESS((usys)address) {
+    }
     
     //! \brief Constructor in the case the address is a standard typed address
-    constexpr Register(const usys address);
+    constexpr Register(const usys address)
+      : ADDRESS(address) {
+    }
 
     // Assignment
 #define MACRO_DECLARE_ASSIGN(op)					\
-    template<typename OtherRegType, typename OtherGroup, RegType MASK> const Register& operator op(const Config<OtherRegType, OtherGroup, MASK>& cfg) const; \
-    template<RegType MASK> const Register& operator op(const Config<RegType, Group, MASK>& cfg) const; \
-    template<typename OtherRegType, typename OtherGroup> const Register& operator op(const Register<OtherRegType, OtherGroup>& reg) const; \
-    const Register& operator op(const Register<RegType, Group>& reg) const; \
-    const Register& operator op(const RegType val) const;		\
+    template<typename OtherRegType, typename OtherGroup, RegType MASK>  \
+    const Register& operator op(const Config<OtherRegType, OtherGroup, MASK>& cfg) const { \
+      static_assert(!sizeof(RegType) && sizeof(RegType),		\
+		    "Incompatibles Register and Config");		\
+      return *this;							\
+    }									\
+    template<RegType MASK>						\
+    const Register& operator op(const Config<RegType, Group, MASK>& cfg) const { \
+      *(volatile RegType*)ADDRESS op cfg.VALUE;				\
+      return *this;							\
+    }									\
+    template<typename OtherRegType, typename OtherGroup>		\
+    const Register& operator op(const Register<OtherRegType, OtherGroup>& reg) const { \
+      static_assert(!sizeof(RegType) && sizeof(RegType),		\
+		    "Incompatibles Registers");				\
+      return *this;							\
+    }									\
+    const Register& operator op(const Register<RegType, Group>& reg) const { \
+      *(volatile RegType*)ADDRESS op *(RegType*)reg.ADDRESS;		\
+      return *this;							\
+    }									\
+    const Register& operator op(const RegType val) const {		\
+      *(volatile RegType*)ADDRESS op val;				\
+      return *this;							\
+    }									\
 
     MACRO_DECLARE_ASSIGN(=);
     MACRO_DECLARE_ASSIGN(|=);
@@ -45,27 +71,74 @@ namespace MemoryMapping {
     
     // Arithmetic
 #define MACRO_DECLARE_ARITH(op)			\
-    template<typename OtherRegType, typename OtherGroup, RegType MASK> const RegType operator op(const Config<OtherRegType, OtherGroup, MASK>& cfg) const; \
-    template<RegType MASK> const RegType operator op(const Config<RegType, Group, MASK>& cfg) const; \
-    template<typename OtherRegType, typename OtherGroup> const RegType operator op(const Register<OtherRegType, OtherGroup>& reg) const; \
-    const RegType operator op(const Register<RegType, Group>& reg) const; \
-    const RegType operator op(const RegType val) const;
+    template<typename OtherRegType, typename OtherGroup, RegType MASK>	\
+    const RegType operator op(const Config<OtherRegType, OtherGroup, MASK>& cfg) const { \
+      static_assert(!sizeof(RegType) && sizeof(RegType),		\
+		    "Incompatibles Register and Config");		\
+      return 0;								\
+    }									\
+    template<RegType MASK>						\
+    const RegType operator op(const Config<RegType, Group, MASK>& cfg) const { \
+      return *(volatile RegType*)ADDRESS op cfg.VALUE;			\
+    }									\
+    template<typename OtherRegType, typename OtherGroup>		\
+    const RegType operator op(const Register<OtherRegType, OtherGroup>& reg) const { \
+      static_assert(!sizeof(RegType) && sizeof(RegType),		\
+		    "Incompatibles Registers");				\
+      return 0;								\
+    }									\
+    const RegType operator op(const Register<RegType, Group>& reg) const { \
+      return								\
+	*(volatile RegType*)ADDRESS op *(volatile RegType*)reg.ADDRESS; \
+    }									\
+    const RegType operator op(const RegType val) const {		\
+      return *(volatile RegType*)ADDRESS op val;			\
+    }
 
     MACRO_DECLARE_ARITH(|);
     MACRO_DECLARE_ARITH(&);
     MACRO_DECLARE_ARITH(^);
 
-    const RegType operator>>(const RegType val) const;
-    const RegType operator<<(const RegType val) const;
-
 #undef MACRO_DECLARE_ARITH
 
-    const RegType operator~(void) const;
+    const RegType operator>>(const RegType val) const {
+      return *(volatile RegType*)ADDRESS >> val;
+    }
+    
+    const RegType operator<<(const RegType val) const {
+      return *(volatile RegType*)ADDRESS << val;
+    }
+
+    const RegType operator~(void) const {
+      return ~(*(volatile RegType*)ADDRESS);
+    }
+
+#define MACRO_DECLARE_COMPAR(op)					\
+    bool operator op(const Register& reg) const {			\
+      return VAL(*this) op VAL(reg);					\
+    }									\
+    template<RegType MASK>						\
+    bool operator op(const Config<RegType, Group, MASK>& cfg) const {	\
+      static_assert(MASK == Integer<sizeof(RegType)*8>::MAX_UNSIGNED,	\
+		    "Can't compare a partial config to this register");	\
+      return VAL(*this) op VAL(cfg);					\
+    }									\
+    bool operator op(const RegType val) const {				\
+      return VAL(*this) op val;						\
+    }
+    
+    MACRO_DECLARE_COMPAR(==);
+    MACRO_DECLARE_COMPAR(!=);
+    
+#undef MACRO_DECLARE_COMPAR
+    
   };
 
   //! \brief Return the Register's value
   template<typename RegType, typename Group>
-  inline const RegType VAL(const Register<RegType, Group>& reg);
+  inline const RegType VAL(const Register<RegType, Group>& reg) {
+    return *(volatile RegType*)reg.ADDRESS;
+  }
 }
 
 #endif//REGISTER_HPP
