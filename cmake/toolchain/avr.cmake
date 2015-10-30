a89bcb3190605719232dc07e40976e910cfb3643
Elioty <roadkiller.cl@gmail.com>
2015-10-30 19:45:56 +0100

    Minor change to CMake toolchain files.

--- cmake/toolchain/avr.cmake
+++ cmake/toolchain/avr.cmake
@@ -8,10 +8,13 @@ include("${CMAKE_CURRENT_LIST_DIR}/generic.cmake")
 set(AVR_FLAGS "-Os -mcall-prologues -I${CMAKE_CURRENT_SOURCE_DIR}/include/avr -mmcu=${AVERSIVE_MCU}")
 
 set(AVERSIVE_PLATFORM    avr)
+
 set(CMAKE_C_COMPILER     avr-gcc)
 set(CMAKE_C_FLAGS_INIT   "${CMAKE_C_FLAGS_INIT} ${AVR_FLAGS}")
+
 set(CMAKE_CXX_COMPILER   avr-g++)
 set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} ${AVR_FLAGS} -fno-rtti -fno-exceptions")
+
 set(CMAKE_ASM_COMPILER   avr-gcc)
 set(CMAKE_ASM_FLAGS_INIT "${CMAKE_ASM_FLAGS_INIT} ${AVR_FLAGS}")
 
